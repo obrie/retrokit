5326230b1de8e43335d1d6b59f494687e840c703
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-03-15 00:00:00 -0400

    changes

--- config/platforms/ports/roms.sh
+++ config/platforms/ports/roms.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
+set -ex
+
+##############
+# Cannonball
+##############
+
+sudo ~/RetroPie-Setup/retropie_packages.sh cannonball _binary_
+
 ##############
 # Duke Nukem 3D
 ##############
 
-set -e
-
 sudo ~/RetroPie-Setup/retropie_packages.sh eduke32 _binary_
 
 # TODO: Switch Renderer to "Classic"
+
+##############
+# Quake
+##############
+
+sudo ~/RetroPie-Setup/retropie_packages.sh quake _binary_
+
+##############
+# Wolfenstein 3D
+##############
+
+sudo ~/RetroPie-Setup/retropie_packages.sh wolf4sdl _binary_
