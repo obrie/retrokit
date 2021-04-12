681da0c793c8c5a07052a00d9f7d5a4cb94f2671
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-04-12 00:00:00 -0400

    Bluetooth backup

--- bin/setup/bluetooth.sh
+++ bin/setup/bluetooth.sh
@@ -6,6 +6,11 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/../common.sh"
 
 install() {
+  # Back up bluetooth settings
+  if [ ! -d '/var/lib/bluetooth.orig' ]; then
+    sudo cp -R /var/lib/bluetooth/ /var/lib/bluetooth.orig
+  fi
+
   sudo cp -R $config_dir/bluetooth/* /var/lib/bluetooth/
 }
 
