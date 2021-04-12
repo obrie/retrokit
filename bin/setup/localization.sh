13f0c0e385bdd40b3216bab1f3234c0313e1c48c
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-04-12 00:00:00 -0400

    Sudo

--- bin/setup/localization.sh
+++ bin/setup/localization.sh
@@ -13,19 +13,19 @@ reconfigure() {
 }
 
 setup() {
-  conf_cp "$config_dir/localization/locale" '/etc/default/locale'
-  conf_cp "$config_dir/localization/locale.gen" '/etc/locale.gen'
-  conf_cp "$config_dir/localization/timezone" '/etc/timezone'
-  env_merge "$config_dir/localization/keyboard" '/etc/default/keyboard'
+  conf_cp "$config_dir/localization/locale" '/etc/default/locale' as_sudo=true
+  conf_cp "$config_dir/localization/locale.gen" '/etc/locale.gen' as_sudo=true
+  conf_cp "$config_dir/localization/timezone" '/etc/timezone' as_sudo=true
+  env_merge "$config_dir/localization/keyboard" '/etc/default/keyboard' as_sudo=true
 
   reconfigure
 }
 
 uninstall() {
-  restore '/etc/default/locale'
-  restore '/etc/locale.gen'
-  restore '/etc/timezone'
-  restore '/etc/default/keyboard'
+  restore '/etc/default/locale' as_sudo=true
+  restore '/etc/locale.gen' as_sudo=true
+  restore '/etc/timezone' as_sudo=true
+  restore '/etc/default/keyboard' as_sudo=true
 
   reconfigure
 }
