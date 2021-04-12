ef0c55356615250fb18c3b6b3313e02e4878b8c2
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-04-12 00:00:00 -0400

    Typo

--- bin/setup/vnc.sh
+++ bin/setup/vnc.sh
@@ -11,14 +11,14 @@ install() {
   echo 'deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main' | sudo tee /etc/apt/sources.list.d/uv4l.list
 
   # Install sever
-  sudo apt install -y uv4luv4l-server uv4l-webrtc uv4l-raspidisp uv4l-raspidisp-extras
+  sudo apt install -y uv4l uv4l-server uv4l-webrtc uv4l-raspidisp uv4l-raspidisp-extras
 
   # Enable WebRTC access
   uv4l --auto-video_nr --driver raspidisp --server-option '--enable-webrtc=yes'
 }
 
 uninstall() {
-  sudo apt remove -y uv4luv4l-server uv4l-webrtc uv4l-raspidisp uv4l-raspidisp-extras
+  sudo apt remove -y uv4l uv4l-server uv4l-webrtc uv4l-raspidisp uv4l-raspidisp-extras
 }
 
 "${@}"
