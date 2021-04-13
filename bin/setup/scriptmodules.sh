f369ce6d196f11d93f576bd6a3b9068fa4041100
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-04-13 00:00:00 -0400

    Fix

--- bin/setup/scriptmodules.sh
+++ bin/setup/scriptmodules.sh
@@ -6,7 +6,7 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/../common.sh"
 
 install() {
-  cp -R "$config_dir/scriptmodules/*" "$HOME/RetroPie-Setup/scriptmodules/"
+  cp -R "$config_dir/scriptmodules/" "$HOME/RetroPie-Setup/"
 }
 
 uninstall() {
