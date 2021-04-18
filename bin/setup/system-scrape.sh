89de2458a1a2c4db8fc30beb22fba24792bde11b
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-04-18 00:00:00 -0400

    Try array expansion

--- bin/setup/system-scrape.sh
+++ bin/setup/system-scrape.sh
@@ -7,12 +7,12 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 scrape() {
   local source="$1"
-  local extra_args=$(system_setting '.scraper.args?')
+  local extra_args=($(system_setting '.scraper.args?'))
 
   stop_emulationstation
 
   log "Scaping $system from $source"
-  /opt/retropie/supplementary/skyscraper/Skyscraper -p "$system" -s "$source" --flags onlymissing $extra_args
+  /opt/retropie/supplementary/skyscraper/Skyscraper -p "$system" -s "$source" --flags onlymissing ${extra_args[@]}
 }
 
 scrape_sources() {
@@ -22,10 +22,10 @@ scrape_sources() {
 }
 
 build_gamelist() {
-  local extra_args=$(system_setting '.scraper.args?')
+  local extra_args=($(system_setting '.scraper.args?'))
 
   log "Building gamelist for $system"
-  /opt/retropie/supplementary/skyscraper/Skyscraper -p "$system" $extra_args
+  /opt/retropie/supplementary/skyscraper/Skyscraper -p "$system" ${extra_args[@]}
 }
 
 install() {
