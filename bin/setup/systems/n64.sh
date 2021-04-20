1f3d79ae1bff29b5fb6439ee60cc5c6a75286979
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-04-20 00:00:00 -0400

    Proper ini merge

---
+++ bin/setup/systems/n64.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+system='pc'
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+. "$dir/../system-common.sh"
+
+install() {
+  ini_merge "$system_config_dir/mupen64plus.cfg" '/opt/retropie/configs/n64/mupen64plus.cfg'
+}
+
+uninstall() {
+  restore '/opt/retropie/configs/n64/mupen64plus.cfg'
+}
+
+"${@}"
