f0fd84fa5df512172893e22169dd609ea6bca252
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-04-20 00:00:00 -0400

    Fix backup/restore process for emulators.cfg

--- bin/setup/system-emulators.sh
+++ bin/setup/system-emulators.sh
@@ -7,21 +7,23 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 # Install emulators
 install_emulators() {
-  backup_and_restore "$retropie_system_config_dir/emulators.cfg"
-
-  while IFS="$tab" read -r emulator build branch is_default; do
+  # Install packages
+  while IFS="$tab" read -r emulator build branch; do
     local package_type='emulators'
     if [[ "$emulator" == lr-* ]]; then
       package_type='libretrocores'
     fi
 
     install_retropie_package "$package_type" "$emulator" "$build" "$branch"
+  done < <(system_setting '.emulators | to_entries[] | [.key, .value.build // "binary", .value.branch // "master"] | @tsv')
 
-    # Set default
+  # Set defaults
+  backup "$retropie_system_config_dir/emulators.cfg"
+  while IFS="$tab" read -r emulator is_default; do
     if [ "$is_default" == "true" ]; then
       crudini --set "$retropie_system_config_dir/emulators.cfg" '' 'default' "\"$emulator\""
     fi
-  done < <(system_setting '.emulators | to_entries[] | [.key, .value.build // "binary", .value.branch // "master", .value.default // false] | @tsv')
+  done < <(system_setting '.emulators | to_entries[] | [.key, .value.default // false] | @tsv')
 }
 
 # Install BIOS files required by emulators
