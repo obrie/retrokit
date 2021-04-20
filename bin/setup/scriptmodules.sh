1ce9314e21a23875bdf48caca2433b92c7d1f258
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-04-20 00:00:00 -0400

    Fix pc build for sdl2, fix scriptmodules conflict

--- bin/setup/scriptmodules.sh
+++ bin/setup/scriptmodules.sh
@@ -6,7 +6,16 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/../common.sh"
 
 install() {
-  cp -R "$config_dir/scriptmodules/*" "$HOME/RetroPie-Setup/scriptmodules/"
+  while read -r filepath; do
+    local source_path="$config_dir/scriptmodules/$filepath"
+    local target_path="$HOME/RetroPie-Setup/scriptmodules/$filepath"
+
+    # Remove any backup files for the scriptmodule
+    rm -f "$target_path.orig"
+
+    # Copy over the scriptmodule
+    cp "$source_path" "$target_path"
+  done < <(find "$config_dir/scriptmodules" -type f -printf '%P\n')
 }
 
 uninstall() {
