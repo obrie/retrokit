27e7f138a2e593b52fcd315c0b0c1902cb7cd454
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-04-21 00:00:00 -0400

    Integrate runcommand cfg to disable the menu

--- bin/setup/runcommand.sh
+++ bin/setup/runcommand.sh
@@ -8,9 +8,12 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 install() {
   file_ln "$app_dir/bin/runcommand/onstart.sh" '/opt/retropie/configs/all/runcommand-onstart.sh'
   file_ln "$app_dir/bin/runcommand/onend.sh" '/opt/retropie/configs/all/runcommand-onend.sh'
+
+  ini_merge "$config_dir/runcommand/runcommand.cfg" '/opt/retropie/configs/all/runcommand.cfg'
 }
 
 uninstall() {
+  restore '/opt/retropie/configs/all/runcommand.cfg'
   restore '/opt/retropie/configs/all/runcommand-onstart.sh'
   restore '/opt/retropie/configs/all/runcommand-onend.sh'
 }
