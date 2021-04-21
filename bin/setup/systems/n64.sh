e125f5529060e3dfc5f759ee8980015e1816994e
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-04-21 00:00:00 -0400

    Fix n64 controller to use d pad

--- bin/setup/systems/n64.sh
+++ bin/setup/systems/n64.sh
@@ -8,10 +8,12 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 install() {
   ini_merge "$system_config_dir/mupen64plus.cfg" '/opt/retropie/configs/n64/mupen64plus.cfg'
+  ini_merge "$system_config_dir/InputAutoCfg.ini" '/opt/retropie/configs/n64/InputAutoCfg.ini'
 }
 
 uninstall() {
   restore '/opt/retropie/configs/n64/mupen64plus.cfg'
+  restore '/opt/retropie/configs/n64/InputAutoCfg.ini'
 }
 
 "${@}"
