4a74bb76f6ec9046cc70807175985c5a8d724cb4
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-04-21 00:00:00 -0400

    Move port configuration to the settings.json file

--- bin/setup/systems/ports.sh
+++ bin/setup/systems/ports.sh
@@ -7,24 +7,15 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/../system-common.sh"
 
 install() {
-  # Duke 3D
-  install_retropie_package 'ports' eduke32
-  if [ -d "$HOME/RetroPie/roms/pc/.exodos/Duke3D" ]; then
-    file_ln "$HOME/RetroPie/roms/pc/.exodos/Duke3D/DUKE3D/DUKE3D.GRP" "$HOME/RetroPie/roms/ports/duke3d/duke3d.grp"
-    file_ln "$HOME/RetroPie/roms/pc/.exodos/Duke3D/DUKE3D/DUKE.RTS" "$HOME/RetroPie/roms/ports/duke3d/duke.rts"
-  fi
+  while IFS="$tab" read -r port_name package_name package_type; do
+    install_retropie_package "$package_type" "$package_name"
 
-  # Quake
-  install_retropie_package 'libretrocores' lr-tyrquake
-  if [ -d "$HOME/RetroPie/roms/pc/.exodos/Quake" ]; then
-    file_ln "$HOME/RetroPie/roms/pc/.exodos/Quake/QUAKE/ID1/PAK1.PAK" "$HOME/RetroPie/roms/ports/quake/id1/pak1.pak"
-  fi
-
-  # Doom
-  install_retropie_package 'libretrocores' lr-prboom
-  if [ -d "$HOME/RetroPie/roms/pc/.exodos/DOOM" ]; then
-    file_ln "$HOME/RetroPie/roms/pc/.exodos/DOOM/DOOM.WAD" "$HOME/RetroPie/roms/ports/doom/doom.wad"
-  fi
+    # Link over any optional files for the game
+    while read -r target_name source_path; do
+      local target_path="$HOME/RetroPie/roms/ports/$port_name/$target_name"
+      file_ln "$source_path" "$target_path"
+    done < <(system_setting '.ports.$port_name.files | try to_entries[] | [.key, .value] | @tsv')
+  done < <(system_setting '.ports | to_entries[] | [.key, .value.package, .value.package_type] | @tsv')
 }
 
 uninstall() {
