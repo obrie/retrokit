f384d79bb1a15bd4af06cea41db083e59d535f5d
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-04-25 00:00:00 -0400

    Allow any system to disable launch images

--- bin/setup/system-launchimages.sh
+++ bin/setup/system-launchimages.sh
@@ -6,12 +6,12 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/system-common.sh"
 
 install() {
-  local launch_theme=$(setting '.themes.launch_theme')
-  local launch_images_base_url=$(setting ".themes.library[] | select(.name == \"$launch_theme\") | .launch_images_base_url")
-  local system_theme=$(system_setting '.themes.system // .system')
-  
-  if [ "$system" != 'pc' ]; then
-    # Currently no support for PC launch images due to keyboard issues
+  local use_launch_image=$(system_setting '.themes.launch_image // true')
+  if [ "$use_launch_image" == 'true' ]; then
+    local launch_theme=$(setting '.themes.launch_theme')
+    local launch_images_base_url=$(setting ".themes.library[] | select(.name == \"$launch_theme\") | .launch_images_base_url")
+    local system_theme=$(system_setting '.themes.system // .system')
+    
     download "$(printf "$launch_images_base_url" "$system_theme")" "$retropie_system_config_dir/launching-extended.png"
   fi
 }
