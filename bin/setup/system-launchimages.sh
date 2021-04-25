45650b956d77a67c1eef805d8151d6fdb0e10542
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-04-25 00:00:00 -0400

    Fix launchimages to have overrides for any system

--- bin/setup/system-launchimages.sh
+++ bin/setup/system-launchimages.sh
@@ -6,17 +6,13 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/system-common.sh"
 
 install() {
-  launch_theme=$(setting '.themes.launch_theme')
-  launch_images_base_url=$(setting ".themes.library[] | select(.name == \"$launch_theme\") | .launch_images_base_url")
-
-  local system_image_name=$system
-  if [ "$system_image_name" == "megadrive" ]; then
-    system_image_name="genesis"
-  fi
+  local launch_theme=$(setting '.themes.launch_theme')
+  local launch_images_base_url=$(setting ".themes.library[] | select(.name == \"$launch_theme\") | .launch_images_base_url")
+  local system_theme=$(system_setting '.themes.system // .system')
   
   if [ "$system" != 'pc' ]; then
     # Currently no support for PC launch images due to keyboard issues
-    download "$(printf "$launch_images_base_url" "$system_image_name")" "$retropie_system_config_dir/launching-extended.png"
+    download "$(printf "$launch_images_base_url" "$system_theme")" "$retropie_system_config_dir/launching-extended.png"
   fi
 }
 
