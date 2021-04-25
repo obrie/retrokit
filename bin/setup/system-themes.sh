024e52eb44ba20dd9d37c5a5e844eba24281faff
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-04-25 00:00:00 -0400

    Also update platform

--- bin/setup/system-themes.sh
+++ bin/setup/system-themes.sh
@@ -10,6 +10,7 @@ install() {
 
   if [ -n "$system_theme" ]; then
     xmlstarlet ed -L -u "systemList/system[name=\"$system\"]/theme" -v "$system_theme" "$HOME/.emulationstation/es_systems.cfg"
+    xmlstarlet ed -L -u "systemList/system[name=\"$system\"]/platform" -v "$system_theme" "$HOME/.emulationstation/es_systems.cfg"
   fi
 }
 
@@ -17,6 +18,7 @@ uninstall() {
   local system_theme=$(system_setting '.themes.system')
 
   if [ -n "$system_theme" ]; then
+    xmlstarlet ed -L -u "systemList/system[name=\"$system_theme\"]/platform" -v "$system" "$HOME/.emulationstation/es_systems.cfg"
     xmlstarlet ed -L -u "systemList/system[name=\"$system_theme\"]/theme" -v "$system" "$HOME/.emulationstation/es_systems.cfg"
   fi
 }
