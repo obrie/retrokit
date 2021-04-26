b4dc58f5806f87dd0afd1ada6ee067ef5802f5e6
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-04-26 00:00:00 -0400

    Initial collections support

--- bin/setup/system-scrape.sh
+++ bin/setup/system-scrape.sh
@@ -28,9 +28,39 @@ build_gamelist() {
   /opt/retropie/supplementary/skyscraper/Skyscraper -p "$system" ${extra_args[@]}
 }
 
+build_collections() {
+  local source_collections_dir="$config_dir/emulationstation/collections"
+  local target_collections_dir="$HOME/.emulationstation/collections"
+  mkdir -p "$target_collections_dir"
+
+  while read -r filename; do
+    # Identify what collection we're dealing with
+    local collection_name=$(basename "$filename" '.tsv')
+    local source_collection_path="$source_collections_dir/$filename"
+    local target_collection_path="$target_collections_dir/$collection_name.cfg"
+
+    # Remove any existing entries for this system from the collection
+    if [ -f "$target_collection_path" ]; then
+      sed -i "/^$system,/d" "$target_collection_path"
+    fi
+
+    while IFS="$tab" read -r collection_system machine_name; do
+      if [ "$collection_system" == "$system" ]; then
+        local machine_path=$(find "$HOME/RetroPie/roms/$system" -type l -name "$machine_name*" | head -n 1)
+
+        # If path is found, add it
+        if [ -n "$machine_path" ]; then
+          echo "$machine_path" >> "$target_collection_path"
+        fi
+      fi
+    done < "$source_collection_path"
+  done < <(ls "$source_collections_dir")
+}
+
 install() {
   scrape_sources
   build_gamelist
+  build_collections
 }
 
 uninstall() {
