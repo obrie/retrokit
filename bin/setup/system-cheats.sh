161d709de1af81b7c00741fc5df5f9b8d20bd32a
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-01 00:00:00 -0400

    Attempt to fix cheat path matching

--- bin/setup/system-cheats.sh
+++ bin/setup/system-cheats.sh
@@ -10,22 +10,43 @@ install() {
   local cheats_name=$(system_setting '.cheats')
 
   if [ -n "$cheats_name" ]; then
-    local cheats_dir="$retroarch_config_dir/cheats"
+    # Location of the cheats on the filesystem
+    local cheat_database_path="$retroarch_config_dir/cheats"
+    local source_cheats_dir="$cheat_database_path/$cheats_name"
 
     # Get cheat database path for this system
-    local system_cheats_dir=$(crudini --get "$retropie_system_config_dir/retroarch.cfg" '' 'cheat_database_path' 2>/dev/null || true)
-    system_cheats_dir="${system_cheats_dir//\"}"
-    if [ -z "$system_cheats_dir" ]; then
-      system_cheats_dir="$cheats_dir"
+    local system_cheat_database_path=$(crudini --get "$retropie_system_config_dir/retroarch.cfg" '' 'cheat_database_path' 2>/dev/null || true)
+    system_cheat_database_path="${system_cheat_database_path//\"}"
+    if [ -z "$system_cheat_database_path" ]; then
+      system_cheat_database_path="$cheat_database_path"
     fi
-    mkdir -p "$system_cheats_dir"
+    mkdir -p "$system_cheat_database_path"
 
     # Link the named Retroarch cheats to the emulator in the system cheats namespace
     while IFS="$tab" read emulator library_name; do
-      local emulator_cheats_dir="$system_cheats_dir/$library_name"
+      local target_cheats_dir="$system_cheat_database_path/$library_name"
 
-      rm -rf "$emulator_cheats_dir"
-      ln -fs "$cheats_dir/$cheats_name" "$emulator_cheats_dir"
+      # Reset the directory
+      rm -rf "$target_cheats_dir"
+      mkdir -p "$target_cheats_dir"
+
+      # We can't just symlink to the source directory because the cheat filenames
+      # don't always match the ROM names.  As a result, we need to try to do some
+      # smart matching to find the corresponding cheat file.
+      while IFS= read -r rom_filename; do
+        # Find a matching cheat by:
+        # * Exact name match
+        # * Inclusive name match
+        # * Exact title match
+        # * Inclusive title match
+        rom_name="${rom_filename%.*}"
+        rom_title="${rom_filename%% \(*}"
+        rom_cheat_path=$(find "$source_cheats_dir" -name "$rom_name.cht" -o -name "$rom_name*.cht" -o -name "$rom_title.cht" -o -name "$rom_title (*.cht" | head -n 1)
+
+        if [ -n "$rom_cheat_path" ]; then
+          ln -fs "$rom_cheat_path" "$target_cheats_dir/$rom_name.cht"
+        fi
+      done < <(find "$HOME/RetroPie/roms/$system" -type l -printf '"%p"\n' | xargs -I{} basename "{}" | sort | uniq)
     done < <(system_setting '.emulators | try to_entries[] | select(.value.library_name) | [.key, .value.library_name] | @tsv')
   fi
 }
