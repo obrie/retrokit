56903deb0bb7118d9f8250c800928a8deedca847
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-04 00:00:00 -0400

    Unify remaps, retroarch, and retroarch_opts directories since we can differentiate on extension

--- bin/setup/system-retroarch.sh
+++ bin/setup/system-retroarch.sh
@@ -22,27 +22,27 @@ install_global_core_options() {
 
 # Game-specific core options
 install_game_core_options() {
-  if [ -d "$system_config_dir/retroarch_opts" ]; then
+  if [ -d "$system_config_dir/retroarch" ]; then
     while read library_name core_name; do
       # Retroarch emulator-specific config
       local retroarch_emulator_config_dir="$retroarch_config_dir/config/$library_name"
       mkdir -p "$retroarch_emulator_config_dir"
 
       # Core Options overides (https://retropie.org.uk/docs/RetroArch-Core-Options/)
-      find "$system_config_dir/retroarch_opts" -iname "*.opt" | while read override_file; do
+      while read override_file; do
         local opt_name=$(basename "$override_file")
         local opt_file="$retroarch_emulator_config_dir/$opt_name"
         
         grep -E "^$core_name" /opt/retropie/configs/all/retroarch-core-options.cfg > "$opt_file"
         crudini --merge "$opt_file" < "$override_file"
-      done
+      done < <(find "$system_config_dir/retroarch" -iname "*.opt")
     done < <(system_setting '.emulators | to_entries[] | select(.value.core_name) | [.value.library_name, .value.core_name] | @tsv')
   fi
 }
 
 # Games-specific controller remappings
 install_game_remappings() {
-  if [ -d "$system_config_dir/remaps" ]; then
+  if [ -d "$system_config_dir/retroarch" ]; then
     local remapping_dir=$(crudini --get "$retropie_system_config_dir/retroarch.cfg" '' 'input_remapping_directory' 2>/dev/null || true)
     if [ -z "$remapping_dir" ]; then
       echo 'Not implemented'
@@ -57,7 +57,7 @@ install_game_remappings() {
       while read -r remap_path; do
         local remap_filename=$(basename "$remap_path")
         cp "$remap_path" "$emulator_remapping_dir/$remap_filename"
-      done < <(find "$system_config_dir/remaps" -iname "*.rmp")
+      done < <(find "$system_config_dir/retroarch" -iname "*.rmp")
     done < <(system_setting '.emulators | to_entries[] | select(.value.core_name) | [.value.library_name]')
   fi
 }
