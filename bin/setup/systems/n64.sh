97bbd5b510a330c6f437e86e1fc93814c1c44e50
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-04 00:00:00 -0400

    Merge glide config

--- bin/setup/systems/n64.sh
+++ bin/setup/systems/n64.sh
@@ -7,13 +7,15 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/../system-common.sh"
 
 install() {
+  ini_merge "$system_config_dir/GLideN64.custom.ini" '/opt/retropie/configs/n64/GLideN64.custom.ini'
   ini_merge "$system_config_dir/mupen64plus.cfg" '/opt/retropie/configs/n64/mupen64plus.cfg'
   ini_merge "$system_config_dir/InputAutoCfg.ini" '/opt/retropie/configs/n64/InputAutoCfg.ini'
 }
 
 uninstall() {
-  restore '/opt/retropie/configs/n64/mupen64plus.cfg'
   restore '/opt/retropie/configs/n64/InputAutoCfg.ini'
+  restore '/opt/retropie/configs/n64/mupen64plus.cfg'
+  restore '/opt/retropie/configs/n64/GLideN64.custom.ini'
 }
 
 "${@}"
