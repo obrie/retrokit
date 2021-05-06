a135172c637e7a88a06de38a35277ff41a5e0151
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-06 00:00:00 -0400

    Initial fix for 8bitdo ghost inputs

--- bin/setup/bluetooth.sh
+++ bin/setup/bluetooth.sh
@@ -5,7 +5,7 @@ set -ex
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/../common.sh"
 
-install() {
+install_pairings() {
   # Back up bluetooth settings
   if [ ! -d '/var/lib/bluetooth.rk-src' ]; then
     sudo cp -R /var/lib/bluetooth/ /var/lib/bluetooth.rk-src
@@ -14,8 +14,20 @@ install() {
   sudo cp -R $config_dir/bluetooth/* /var/lib/bluetooth/
 }
 
+# Fix ghost inputs on initial connection
+# See: https://retropie.org.uk/docs/Bluetooth-Controller/
+fix_ghost_inputs() {
+  backup_and_restore /usr/bin/btuart
+  sed -i 's/bcm43xx 921600/bcm43xx 115200/g'
+}
+
+install() {
+  install_pairings
+  fix_ghost_inputs
+}
+
 uninstall() {
-  echo 'Uninstall skipped for bluetooth'
+  restore /usr/bin/btuart
 }
 
 "${@}"
