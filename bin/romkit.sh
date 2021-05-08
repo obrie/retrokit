202fbd2ee8bf841c1a86f51313e17ef9859a723b
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-08 00:00:00 -0400

    Add bin/romkit.sh for easily directly interacting with romkit

---
+++ bin/romkit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -ex
+
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+. "$dir/common.sh"
+
+system="$1"
+system_settings_file="$app_dir/config/systems/$system/settings.json"
+
+romkit_cli() {
+  TMPDIR="$tmp_dir" python3 bin/romkit/cli.py $1 "$system_settings_file" ${@:2}
+}
+
+list() {
+  romkit_cli list --log-level ERROR
+}
+
+vacuum() {
+  romkit_cli vacuum --log-level ERROR
+}
+
+"$2" "${@:3}"
