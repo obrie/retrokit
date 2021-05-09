661a3b21f77fed84faa799f135de1dc0b56eca13
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-09 00:00:00 -0400

    Fix download urls

--- bin/setup/system-roms-overlays.sh
+++ bin/setup/system-roms-overlays.sh
@@ -10,10 +10,15 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 install() {
   # Base URL for downloading overlays
   local bezelproject_name=$(system_setting '.themes.bezel')
-  local bezelproject_url="https://github.com/thebezelproject/bezelproject-$bezelproject_name/raw/master/retroarch"
+  local bezelproject_base_url="https://github.com/thebezelproject/bezelproject-$bezelproject_name/raw/master/retroarch"
+  local bezelproject_overlay_path="overlay/GameBezels/$bezelproject_name"
+  if [ "$system" == 'arcade' ]; then
+    # Arcade override for unknown reasons
+    bezelproject_overlay_path='overlay/ArcadeBezels'
+  fi
 
   # The directory to which we'll install the configurations and images
-  local overlays_dir="$retroarch_config_dir/overlay/GameBezels-$bezelproject_name"
+  local overlays_dir="$retroarch_config_dir/overlay/GameBezels/$bezelproject_name"
 
   # Map emulator to library name
   local default_emulator=""
@@ -30,8 +35,8 @@ install() {
     # Copy over the default overlay
     local input_overlay=$(crudini --get "$retropie_system_config_dir/retroarch.cfg" '' 'input_overlay')
     local input_overlay_name=$(basename "$input_overlay" .cfg)
-    download "$bezelproject_url/overlay/$input_overlay_name.cfg" "$retroarch_config_dir/overlay/$input_overlay_name.cfg"
-    download "$bezelproject_url/overlay/$input_overlay_name.png" "$retroarch_config_dir/overlay/$input_overlay_name.png"
+    download "$bezelproject_base_url/overlay/$input_overlay_name.cfg" "$retroarch_config_dir/overlay/$input_overlay_name.cfg"
+    download "$bezelproject_base_url/overlay/$input_overlay_name.png" "$retroarch_config_dir/overlay/$input_overlay_name.png"
 
     # Download overlays for the associated emulator
     while IFS="$tab" read rom_name emulator; do
@@ -44,8 +49,8 @@ install() {
       # Make sure this is a libretro core
       if [ -n "$library_name" ]; then
         # Install overlay
-        download "$bezelproject_url/overlay/GameBezels-$bezelproject_name/$rom_name.cfg" "$overlays_dir/$rom_name.cfg"
-        download "$bezelproject_url/overlay/GameBezels-$bezelproject_name/$rom_name.png" "$overlays_dir/$rom_name.png"
+        download "$bezelproject_base_url/$bezelproject_overlay_path/$rom_name.cfg" "$overlays_dir/$rom_name.cfg"
+        download "$bezelproject_base_url/$bezelproject_overlay_path/$rom_name.png" "$overlays_dir/$rom_name.png"
 
         # Link emulator configuration to overlay
         echo "input_overlay = \"$overlays_dir/$rom_name.cfg\"" > "$retroarch_config_dir/config/$library/$rom_name.cfg"
