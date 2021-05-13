daa6e6e27cac0d672d5bf4aebd64406c6da487fb
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-13 00:00:00 -0400

    Integrate with retropie's controller configuration script

--- bin/setup/controllers.sh
+++ bin/setup/controllers.sh
@@ -6,22 +6,14 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/../common.sh"
 
 install() {
-  # EmulationStation input config
-  file_cp "$config_dir/controllers/es_input.cfg" "$HOME/.emulationstation/es_input.cfg"
-
-  # Autoconfig files for Retroarch
-  while read autoconfig_path; do
-    local autoconfig_filename=$(basename "$autoconfig_path")
-    file_cp "$autoconfig_path" "/opt/retropie/configs/all/retroarch/autoconfig/$autoconfig_filename"
-  done < <(find "$config_dir/controllers/autoconfig" -type f)
+  # Run RetroPie autoconfig for each controller input
+  while read input_path; do
+    cp "$input_path" "$HOME/.emulationstation/es_temporaryinput.cfg"
+    /opt/retropie/supplementary/emulationstation/scripts/inputconfiguration.sh
+  done < <(find "$config_dir/controllers/inputs" -name '*.cfg')
 }
 
 uninstall() {
-  while read autoconfig_path; do
-    local autoconfig_filename=$(basename "$autoconfig_path")
-    restore "/opt/retropie/configs/all/retroarch/autoconfig/$autoconfig_filename"
-  done < <(find "$config_dir/controllers/autoconfig" -type f)
-
   restore "$HOME/.emulationstation/es_input.cfg"
 }
 
