3ce008b25ffaab52ada77baf5f02e2e95671b3e2
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-18 00:00:00 -0400

    Remove dkms for now

--- config/dkms/8bitdo-quirks/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rpi_kernel_ver='rpi-5.10.y'
-mkdir -p 'drivers/hid/' 'patches'
-curl -s https://raw.githubusercontent.com/raspberrypi/linux/"\$rpi_kernel_ver"/drivers/hid/hid-input.c -o 'drivers/hid/hid-input.c'
-patch -p1 < 'patches/0001-hid8bitdo-quirks.diff'
