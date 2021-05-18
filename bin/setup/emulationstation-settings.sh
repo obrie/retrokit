ea32970606cde05068e609628f09a5b705a57610
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-18 00:00:00 -0400

    Split emulationstation into multiple setup modules for better control

---
+++ bin/setup/emulationstation-settings.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -ex
+
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+. "$dir/../common.sh"
+
+install() {
+  stop_emulationstation
+
+  local es_settings_config="$HOME/.emulationstation/es_settings.cfg"
+  local overrides_config="$config_dir/emulationstation/es_settings.cfg"
+
+  backup_and_restore "$es_settings_config"
+
+  if [ -f "$es_settings_config" ]; then
+    while read -r xml; do
+      # Get current ES settings
+      existing_settings=$(sed -e '$a</settings>' -e 's/<?xml version="1.0"?>/<settings>/g' "$es_settings_config")
+
+      # Read in overrides
+      name=$(echo "$xml" | xmlstarlet sel -t -v '/*/@name')
+      value=$(echo "$xml" | xmlstarlet sel -t -v '/*/@value')
+
+      # Override in the file
+      echo "$existing_settings" |\
+        xmlstarlet edit --update "/*/*[@name=\"$name\"]/@value" --value "$value" |\
+        xmlstarlet select -t -m '/*/*' -c '.' -n |\
+        sed  -e '1s/^/<?xml version="1.0"?>\n/' \
+        > "$es_settings_config"
+    done < <(sed -e '$a</settings>' -e '1s/^/<settings>/' "$overrides_config" | xmlstarlet select -t -m '/*/*' -c '.' -n)
+  else
+    cp "$overrides_config" "$es_settings_config"
+  fi
+}
+
+uninstall() {
+  restore "$HOME/.emulationstation/es_settings.cfg"
+}
+
+"${@}"
--- bin/setup/emulationstation-systems.sh
+++ bin/setup/emulationstation-systems.sh
@@ -5,34 +5,9 @@ set -ex
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/../common.sh"
 
-install_settings() {
-  local es_settings_config="$HOME/.emulationstation/es_settings.cfg"
-  local overrides_config="$config_dir/emulationstation/es_settings.cfg"
+install() {
+  stop_emulationstation
 
-  backup_and_restore "$es_settings_config"
-
-  if [ -f "$es_settings_config" ]; then
-    while read -r xml; do
-      # Get current ES settings
-      existing_settings=$(sed -e '$a</settings>' -e 's/<?xml version="1.0"?>/<settings>/g' "$es_settings_config")
-
-      # Read in overrides
-      name=$(echo "$xml" | xmlstarlet sel -t -v '/*/@name')
-      value=$(echo "$xml" | xmlstarlet sel -t -v '/*/@value')
-
-      # Override in the file
-      echo "$existing_settings" |\
-        xmlstarlet edit --update "/*/*[@name=\"$name\"]/@value" --value "$value" |\
-        xmlstarlet select -t -m '/*/*' -c '.' -n |\
-        sed  -e '1s/^/<?xml version="1.0"?>\n/' \
-        > "$es_settings_config"
-    done < <(sed -e '$a</settings>' -e '1s/^/<settings>/' "$overrides_config" | xmlstarlet select -t -m '/*/*' -c '.' -n)
-  else
-    cp "$overrides_config" "$es_settings_config"
-  fi
-}
-
-install_systems() {
   # Build system order
   local system_default_config=/etc/emulationstation/es_systems.cfg
   local system_override_config="$HOME/.emulationstation/es_systems.cfg"
@@ -67,14 +42,7 @@ install_systems() {
   done < <(setting '.systems[]')
 }
 
-install() {
-  stop_emulationstation
-  install_settings
-  install_systems
-}
-
 uninstall() {
-  restore "$HOME/.emulationstation/es_settings.cfg"
   restore "$HOME/.emulationstation/es_systems.cfg"
 }
 
