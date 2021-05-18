437c4515d563cae6360d745ecc5194631185f5c6
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-18 00:00:00 -0400

    Only install the cheats we need to

--- bin/setup/systems/arcade/cheats.sh
+++ bin/setup/systems/arcade/cheats.sh
@@ -6,24 +6,47 @@ system='arcade'
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/../../system-common.sh"
 
+has_emulator() {
+  if [ $(system_setting ".emulators | has(\"$1\")") == 'true' ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
 install() {
   # Cheats: FBNeo
-  mkdir -p "$HOME/RetroPie/BIOS/fbneo/cheats"
-  if [ ! -f "$system_tmp_dir/fbneo-cheats.zip" ]; then
-    download 'https://github.com/finalburnneo/FBNeo-cheats/archive/master.zip' "$system_tmp_dir/fbneo-cheats.zip"
-    unzip -j "$system_tmp_dir/fbneo-cheats.zip" 'FBNeo-cheats-master/cheats/*' -d "$HOME/RetroPie/BIOS/fbneo/cheats/"
+  if has_emulator 'lr-fbneo'; then
+    mkdir -p "$HOME/RetroPie/BIOS/fbneo/cheats"
+    if [ ! -f "$system_tmp_dir/fbneo-cheats.zip" ]; then
+      download 'https://github.com/finalburnneo/FBNeo-cheats/archive/master.zip' "$system_tmp_dir/fbneo-cheats.zip"
+      unzip -j "$system_tmp_dir/fbneo-cheats.zip" 'FBNeo-cheats-master/cheats/*' -d "$HOME/RetroPie/BIOS/fbneo/cheats/"
+    fi
   fi
 
   # Cheats: MAME (libretro)
-  download 'https://github.com/libretro/mame2010-libretro/raw/master/metadata/cheat.zip' "$HOME/RetroPie/BIOS/mame2010/cheat.zip"
-  download 'https://github.com/libretro/mame2015-libretro/raw/master/metadata/cheat.7z' "$HOME/RetroPie/BIOS/mame2015/cheat.7z"
-  download 'https://github.com/libretro/mame2016-libretro/raw/master/metadata/cheat.7z' "$HOME/RetroPie/BIOS/mame2016/cheat.7z"
-  download 'https://github.com/amadvance/advancemame/raw/master/support/cheat.dat' "$HOME/RetroPie/BIOS/advmame/cheat.dat"
+  if has_emulator 'lr-mame2010'; then
+    download 'https://github.com/libretro/mame2010-libretro/raw/master/metadata/cheat.zip' "$HOME/RetroPie/BIOS/mame2010/cheat.zip"
+  fi
+
+  if has_emulator 'lr-mame2015'; then
+    download 'https://github.com/libretro/mame2015-libretro/raw/master/metadata/cheat.7z' "$HOME/RetroPie/BIOS/mame2015/cheat.7z"
+  fi
+
+  if has_emulator 'lr-mame2016'; then
+    download 'https://github.com/libretro/mame2016-libretro/raw/master/metadata/cheat.7z' "$HOME/RetroPie/BIOS/mame2016/cheat.7z"
+  fi
+
+  if has_emulator 'advmame'; then
+    download 'https://github.com/amadvance/advancemame/raw/master/support/cheat.dat' "$HOME/RetroPie/BIOS/advmame/cheat.dat"
+  fi
 
   # Cheats: MAME (Pugsy)
-  if [ ! -f "$system_tmp_dir/mame-cheats.zip" ]; then
-    download 'http://cheat.retrogames.com/download/cheat0221.zip' "$system_tmp_dir/mame-cheats.zip"
-    unzip -j "$system_tmp_dir/mame-cheats.zip" "cheat.7z" -d "$HOME/RetroPie/BIOS/mame/"
+  if has_emulator 'mame'; then
+    if [ ! -f "$system_tmp_dir/mame-cheats.zip" ]; then
+      download 'http://cheat.retrogames.com/download/cheat0221.zip' "$system_tmp_dir/mame-cheats.zip"
+      unzip -j "$system_tmp_dir/mame-cheats.zip" "cheat.7z" -d "$HOME/RetroPie/BIOS/mame/"
+    fi
   fi
 }
 
