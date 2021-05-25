69ab38e9472c34bbfe1e7e656d796cd1d33d208e
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-25 00:00:00 -0400

    Fix ir uninstall in setupmodule

--- bin/setup/hardware/ir.sh
+++ bin/setup/hardware/ir.sh
@@ -22,7 +22,7 @@ install() {
 
 uninstall() {
   local keymap_path="$(setting '.hardware.ir.keymap')"
-  local keymap_name=$("$keymap_filepath")
+  local keymap_name=$(basename "$keymap_path")
   rm -f "/etc/rc_keymaps/$keymap_name" /opt/retropie/configs/rc_keymap.cfg
 }
 
