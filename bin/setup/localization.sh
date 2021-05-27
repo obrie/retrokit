4a0b5cf2cacb741af5900bb7c5c25f952ca1f9be
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-27 00:00:00 -0400

    Clean up localization script

--- bin/setup/localization.sh
+++ bin/setup/localization.sh
@@ -6,6 +6,12 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/../common.sh"
 
 reconfigure() {
+  # Update symlinks for the timezone
+  sudo timedatectl set-timezone $(cat /etc/timezone)
+
+  # Update current session settings
+  export LANG=$(cat /etc/default/locale)
+
   # Update based on new configurations
   sudo dpkg-reconfigure -f noninteractive tzdata
   sudo update-locale
@@ -18,20 +24,14 @@ install() {
   file_cp "$config_dir/localization/timezone" '/etc/timezone' as_sudo=true
   env_merge "$config_dir/localization/keyboard" '/etc/default/keyboard' as_sudo=true
 
-  # Update symlinks for the timezone
-  sudo timedatectl set-timezone $(cat "$config_dir/localization/timezone")
-
-  # Update current session settings
-  export LANG=$(cat "$config_dir/localization/locale")
-
   reconfigure
 }
 
 uninstall() {
-  restore '/etc/default/locale' as_sudo=true
-  restore '/etc/locale.gen' as_sudo=true
-  restore '/etc/timezone' as_sudo=true
   restore '/etc/default/keyboard' as_sudo=true
+  restore '/etc/timezone' as_sudo=true
+  restore '/etc/locale.gen' as_sudo=true
+  restore '/etc/default/locale' as_sudo=true
 
   reconfigure
 }
