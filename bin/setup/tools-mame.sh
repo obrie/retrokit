f474f704e03b3aba5b4a652772e08058640a625e
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-27 00:00:00 -0400

    Make sure source directory is gone before we attempt to rebuild from source for tools-mame

--- bin/setup/tools-mame.sh
+++ bin/setup/tools-mame.sh
@@ -11,6 +11,7 @@ install_from_source() {
   sudo apt install -y libfontconfig1-dev qt5-default libsdl2-ttf-dev libxinerama-dev libxi-dev
 
   # Build from source
+  rm -rf "$tmp_dir/mame"
   git clone --depth 1 -b mame0230 https://github.com/mamedev/mame "$tmp_dir/mame"
   pushd "$tmp_dir/mame"
   make NOWERROR=1 ARCHOPTS=-U_FORTIFY_SOURCE PYTHON_EXECUTABLE=python3 TOOLS=1 REGENIE=1
