1e2854974100ecf86ddd71e28a3392197ef1e37d
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-28 00:00:00 -0400

    Use setting instead of jq

--- bin/setup/hardware/controllers.sh
+++ bin/setup/hardware/controllers.sh
@@ -11,13 +11,13 @@ install() {
   # Add autoconfig scripts
   while read autoconfig_name; do
     sudo cp "$bin_dir/controllers/$autoconfig_name.sh" "$configscripts_dir/"
-  done < <(jq '.hardware.controllers.autoconfig[]')
+  done < <(setting '.hardware.controllers.autoconfig[]')
 
   # Run RetroPie autoconfig for each controller input
   while read input_name; do
     cp "$config_dir/controllers/inputs/$input_name.cfg" "$HOME/.emulationstation/es_temporaryinput.cfg"
     /opt/retropie/supplementary/emulationstation/scripts/inputconfiguration.sh
-  done < <(jq '.hardware.controllers.inputs[]')
+  done < <(setting '.hardware.controllers.inputs[]')
 }
 
 uninstall() {
@@ -27,7 +27,7 @@ uninstall() {
   # Remove autoconfig scripts
   while read autoconfig_name; do
     sudo rm -f "$configscripts_dir/$autoconfig_name.sh"
-  done < <(jq '.hardware.controllers.autoconfig[]')
+  done < <(setting '.hardware.controllers.autoconfig[]')
 }
 
 "${@}"
