59d19dfc48c7622fb7bba7891da0e9bd46473194
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-30 00:00:00 -0400

    Add sync_media command to bin/sd.sh

--- bin/sd.sh
+++ bin/sd.sh
@@ -14,7 +14,8 @@ usage() {
   echo " $0 create <device> (run from laptop)"
   echo " $0 backup <device> <backup_dir> (run from laptop)"
   echo " $0 restore <device> <backup_dir> (run from laptop)"
-  echo " $0 sync <path> (run from retropie)"
+  echo " $0 sync <path> (run from laptop/retropie)"
+  echo " $0 sync_media <path> (run from laptop/retropie)"
   exit 1
 }
 
@@ -36,6 +37,22 @@ sync() {
   # This should be the full list of paths that might be modified by the using
   # the arcade or using retrokit
   local paths=(/opt/retropie/ /etc/ /home/pi/)
+
+  for path in "${paths[@]}"; do
+    sudo rsync -av "$path" "$sync_to_path$path" --delete
+  done
+}
+
+sync_media() {
+  local sync_to_path=$1
+
+  # This should be the full list of media paths
+  local paths=(
+    /opt/retropie/configs/all/emulationstation/downloaded_media/
+    /opt/retropie/configs/all/skyscraper/
+    /home/pi/RetroPie/roms/
+    /opt/retropie/configs/all/retroarch/overlay/
+  )
 
   for path in "${paths[@]}"; do
     sudo rsync -av "$path" "$sync_to_path$path" --delete
