9bee9df6311670a9c2b9b06c283f2a932c5f7920
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-05-30 00:00:00 -0400

    Update lr-duckstation-psx

--- bin/scriptmodules/libretrocores/lr-duckstation-psx.sh
+++ bin/scriptmodules/libretrocores/lr-duckstation-psx.sh
@@ -12,33 +12,17 @@
 rp_module_id="lr-duckstation-psx"
 rp_module_desc="PlayStation emulator - Duckstation PSX for libretro"
 rp_module_help="ROM Extensions: .bin .cue .cbn .img .iso .m3u .mdf .pbp .toc .z .znx\n\nCopy your PlayStation roms to $romdir/psx\n\nCopy the required BIOS files\n\nscph5500.bin and\nscph5501.bin and\nscph5502.bin to\n\n$biosdir"
-rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/duckstation/master/LICENSE"
-rp_module_repo="git https://github.com/libretro/duckstation.git master"
+rp_module_licence="GPL3 https://raw.githubusercontent.com/stenzek/duckstation/master/LICENSE"
+rp_module_repo="git https://github.com/stenzek/duckstation.git master"
 rp_module_section="exp"
 rp_module_flags=""
 
-function depends_lr-duckstation-psx() {
-    local depends=(libvulkan-dev libgl1-mesa-dev )
-    getDepends "${depends[@]}"
+function __binary_url_lr-duckstation-psx() {
+    echo "https://www.duckstation.org/libretro/duckstation_libretro_linux_armv7.zip"
 }
 
-function sources_lr-duckstation-psx() {
-    gitPullOrClone
-}
-
-function build_lr-duckstation-psx() {
-    mkdir build
-    cd build
-    cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_LIBRETRO_CORE=ON ..
-    ninja -t clean
-    ninja
-    md_ret_require="$md_build/build/duckstation_libretro.so"
-}
-
-function install_lr-duckstation-psx() {
-    md_ret_files=(
-        'build/duckstation_libretro.so'
-    )
+function install_bin_lr-duckstation-psx() {
+    downloadAndExtract "$(__binary_url_duckstation-psx)" "$md_inst"
 }
 
 function configure_lr-duckstation-psx() {
