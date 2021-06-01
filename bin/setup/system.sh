bfc8dde19737b0a65843f547362852aa3afae7ef
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-06-01 00:00:00 -0400

    Rename print_section to print_heading and clean up the output

--- bin/setup/system.sh
+++ bin/setup/system.sh
@@ -5,7 +5,7 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 while read -r setupmodule; do
   if [[ "$setupmodule" == system-* ]] || [[ "$setupmodule" == systems/$system/* ]]; then
-    print_section "Running $1 for $setupmodule"
+    print_heading "Running $1 for $setupmodule"
     "$dir/$setupmodule.sh" "${@}"
   fi
 done < <(setting '.modules[]')
