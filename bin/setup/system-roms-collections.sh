097a57b16c3b0b2688b5ced0aac6f3871f369cfe
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-06-04 00:00:00 -0400

    Fix installation of rom collections

--- bin/setup/system-roms-collections.sh
+++ bin/setup/system-roms-collections.sh
@@ -25,16 +25,17 @@ install() {
       # 1. Do an initial naive search based on a substring match in the gamelist.xml
       # 2. Do an exact match based on comparing the titles
       while read -r machine_path; do
-        local installed_name=${machine_path%%.*}
+        local machine_filename=${machine_path##*/}
+        local installed_name=${machine_filename%%.*}
         local installed_title=${installed_name%% (*}
 
         # If the titles match, add it
-        if [ "$installed_title" = "$machine_title" ]; then
-          echo "Adding $1 to $target_collection_path"
-          echo "$1" >> "$target_collection_path"
+        if [ "$installed_title" == "$machine_title" ]; then
+          echo "Adding $machine_path to $target_collection_path"
+          echo "$machine_path" >> "$target_collection_path"
           break
         fi
-      done < <(xmlstarlet sel -t -m "*/game[contains(path, \"$machine_title\")]" -v 'path' -n "$HOME/.emulationstation/gamelists/$system/gamelist.xml")
+      done < <(xmlstarlet sel -t -m "*/game[contains(path, \"$machine_title\")]" -v 'path' -n "$HOME/.emulationstation/gamelists/$system/gamelist.xml" | xmlstarlet unesc)
     done < <(grep -E "^$system$tab" "$source_collection_path")
   done < <(ls "$source_collections_dir")
 }
