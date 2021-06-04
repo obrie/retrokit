5185a5fb0bd8c46ed6dd59bbbef83319e955f075
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-06-04 00:00:00 -0400

    Improve performance of system-roms-favorites setup module

--- bin/setup/system-roms-favorites.sh
+++ bin/setup/system-roms-favorites.sh
@@ -10,15 +10,15 @@ install() {
     return
   fi
 
+  # Reset by removing all favorite tags first.  This is much faster than
+  # deleting one-by-one given the size of the file.
+  xmlstarlet ed --inplace -d "/gameList/game/favorite" "$gamelist_file"
+
+  # Then add current favorites
   echo 'Setting favorites...'
-  while IFS="$tab" read rom_name is_favorite; do
-    if [ "$is_favorite" == 'true' ]; then
-      xmlstarlet ed --inplace -s "/gameList/game[not(favorite) and (name=\"$rom_name\" or contains(image, \"/$rom_name.\"))]" -t elem -n 'favorite' -v 'true' "$gamelist_file"
-    else
-      # Make sure there's no favorite tag
-      xmlstarlet ed --inplace -d "/gameList/game[name=\"$rom_name\" or contains(image, \"/$rom_name.\")]/favorite" "$gamelist_file"
-    fi
-  done < <(romkit_cache_list | jq -r '[.name, .favorite] | @tsv')
+  while IFS="$tab" read rom_name; do
+    xmlstarlet ed --inplace -s "/gameList/game[name=\"$rom_name\" or contains(image, \"/$rom_name.\")]" -t elem -n 'favorite' -v 'true' "$gamelist_file"
+  done < <(romkit_cache_list | jq -r 'select(.favorite == true) | .name')
 }
 
 uninstall() {
