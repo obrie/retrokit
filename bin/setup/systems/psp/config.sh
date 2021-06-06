977981a6be8d9a812941bf3c459c004be30042a6
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-06-06 00:00:00 -0400

    Optional override of ppsspp controls.ini

--- bin/setup/systems/psp/config.sh
+++ bin/setup/systems/psp/config.sh
@@ -5,10 +5,12 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/../../system-common.sh"
 
 install() {
+  ini_merge "$system_config_dir/controls.ini" '/opt/retropie/configs/psp/PSP/SYSTEM/controls.ini'
   ini_merge "$system_config_dir/ppsspp.ini" '/opt/retropie/configs/psp/PSP/SYSTEM/ppsspp.ini'
 }
 
 uninstall() {
+  restore '/opt/retropie/configs/psp/PSP/SYSTEM/controls.ini' delete_src=true
   restore '/opt/retropie/configs/psp/PSP/SYSTEM/ppsspp.ini' delete_src=true
 }
 
