854bc6c2d7fc5e023477a29990389e292a3acc10
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-06-06 00:00:00 -0400

    Fix system-launchaimges installing a splashscreen for systems even when configured not to

--- bin/setup/system-launchimages.sh
+++ bin/setup/system-launchimages.sh
@@ -4,8 +4,8 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/system-common.sh"
 
 install() {
-  local use_launch_image=$(system_setting '.themes.launch_image // true')
-  if [ "$use_launch_image" == 'true' ]; then
+  local use_launch_image=$(system_setting '.themes.launch_image')
+  if [ "$use_launch_image" != 'false' ]; then
     local launch_theme=$(setting '.themes.launch_theme')
     local launch_images_base_url=$(setting ".themes.library[] | select(.name == \"$launch_theme\") | .launch_images_base_url")
     local default_platform=$(xmlstarlet select -t -m "*/system[name='$system']" -v 'platform' -n /etc/emulationstation/es_systems.cfg)
@@ -13,6 +13,8 @@ install() {
     platform=${platform//\"/}
     
     download "$(printf "$launch_images_base_url" "$platform")" "$retropie_system_config_dir/launching-extended.png"
+  else
+    uninstall
   fi
 }
 
