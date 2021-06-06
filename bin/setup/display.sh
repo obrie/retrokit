427076917b8f5ceeb2b62662f5fe795fca0d2a6b
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-06-06 00:00:00 -0400

    Ensure font changes are reflected in the current console

--- bin/setup/display.sh
+++ bin/setup/display.sh
@@ -3,13 +3,22 @@
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/../common.sh"
 
+update_console() {
+  sudo /etc/init.d/console-setup.sh restart
+
+  if [ "$(tty | grep -E '/dev/tty[1-6]')" == '' ]; then
+    setupcon -f --force
+  fi
+}
+
 install() {
   env_merge "$config_dir/display/console-setup" '/etc/default/console-setup' as_sudo=true
-  sudo /etc/init.d/console-setup.sh restart
+  update_console
 }
 
 uninstall() {
   restore '/etc/default/console-setup' as_sudo=true delete_src=true
+  update_console
 }
 
 "${@}"
