b6a29d2d7a835906c024b5ceea8a1a3e5f7d86fc
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-06-17 00:00:00 -0400

    Move romkit setup to the romkit directory so that one day it can live on its own

---
+++ bin/romkit/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+install() {
+  # Zip
+  sudo apt install -y zip
+
+  # XML processing
+  sudo apt install -y python3-lxml
+
+  if [ ! `command -v trrntzip` ] || [ ! -f /usr/local/etc/trrntzip.version ] || [ $(git ls-remote 'https://github.com/hydrogen18/trrntzip.git' HEAD | cut -f1) != $(cat /usr/local/etc/trrntzip.version) ]; then
+    # Check out
+    rm -rf '/tmp/trrntzip'
+    git clone --depth 1 https://github.com/hydrogen18/trrntzip.git '/tmp/trrntzip'
+    pushd '/tmp/trrntzip'
+    trrntzip_version=$(git rev-parse HEAD)
+
+    # Compile
+    ./autogen.sh
+    ./configure
+    make
+    sudo make install
+    echo "$trrntzip_version" | sudo tee /usr/local/etc/trrntzip.version
+
+    # Clean up
+    popd
+    rm -rf '/tmp/trrntzip'
+  else
+    echo "trrntzip is already the newest version ($(cat /usr/local/etc/trrntzip.version))"
+  fi
+
+  # CHDMan
+  sudo apt install -y mame-tools
+}
+
+"${@}"
--- bin/setup/romkit.sh
+++ bin/setup/romkit.sh
@@ -4,37 +4,7 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/../common.sh"
 
 install() {
-  # Zip
-  sudo apt install -y zip
-
-  # XML processing
-  sudo apt install -y python3-lxml
-
-  # TorrentZip
-  local trrntzip_version="$(cat /usr/local/etc/trrntzip.version 2>/dev/null || true)"
-  if [ ! `command -v trrntzip` ] || has_newer_commit https://github.com/hydrogen18/trrntzip.git "$trrntzip_version"; then
-    # Check out
-    rm -rf "$tmp_dir/trrntzip"
-    git clone --depth 1 https://github.com/hydrogen18/trrntzip.git "$tmp_dir/trrntzip"
-    pushd "$tmp_dir/trrntzip"
-    trrntzip_version=$(git rev-parse HEAD)
-
-    # Compile
-    ./autogen.sh
-    ./configure
-    make
-    sudo make install
-    echo "$trrntzip_version" | sudo tee /usr/local/etc/trrntzip.version
-
-    # Clean up
-    popd
-    rm -rf "$tmp_dir/trrntzip"
-  else
-    echo "trrntzip is already the newest version ($trrntzip_version)"
-  fi
-
-  # CHDMan
-  sudo apt install -y mame-tools
+  "$bin_dir/romkit/setup.sh install"
 }
 
 uninstall() {
