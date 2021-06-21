66a607739cc5d09ea0e9f170746ac33417e0df37
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-06-21 00:00:00 -0400

    Various config cleanup

--- config/skyscraper/videoconvert.sh
+++ config/skyscraper/videoconvert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Converting video..."
+echo 'Converting video...'
 echo "Source: $1"
 echo "Target: $2"
 
@@ -8,9 +8,9 @@ echo "Target: $2"
 has_yuv420p=$(ffprobe "$1" 2>&1 | grep -c yuv420p)
 
 if [ "$has_yuv420p" -eq 0 ]; then
-  echo "Incorrect source video format detected. Converting video to 420p format..."
+  echo 'Incorrect source video format detected. Converting video to 420p format...'
   ffmpeg -i "$1" -y -pix_fmt yuv420p -acodec copy -strict experimental "$2"
 else
-  echo "Video is in correct 420p format. Copying as is..."
-  cp "$1" "$2"
+  echo 'Video is in correct 420p format. Copying as is...'
+  cp -v "$1" "$2"
 fi
