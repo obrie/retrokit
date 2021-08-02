5db470c326fe564556d99a4de367b95ff29df4cb
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2021-08-02 00:00:00 -0400

    Proper kill of manualkit

--- bin/runcommand/onend.sh
+++ bin/runcommand/onend.sh
@@ -7,6 +7,5 @@ dd if=/dev/zero of=/dev/fb0 &>/dev/null
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 python "$dir/runcommand-tty.py" /dev/tty text
 
-# Kill off triggerhappy and vlc as we no longer need to show manuals
-killall thd || true
-killall vlc || true
+# Kill off manualkit
+sudo pkill -f manualkit/cli.py
