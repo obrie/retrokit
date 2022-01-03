bb8e5e87a175032abf80442aa443248857d30fd0
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-01-03 00:00:00 -0400

    Fix new dosbox config processing

--- bin/romkit/systems/pc/system.py
+++ bin/romkit/systems/pc/system.py
@@ -5,6 +5,7 @@ from romkit.systems.pc.metadata import ExodosMetadata
 
 import configparser
 import json
+import re
 import subprocess
 import zipfile
 from pathlib import Path
@@ -18,6 +19,8 @@ class PCSystem(BaseSystem):
 
     APP_ROOT = Path(__file__).parent.resolve().joinpath('../../../..').resolve()
     CONFIG_ARCHIVE = f'{APP_ROOT}/cache/exodos/dosbox-cfg.zip'
+
+    AUTOEXEC_OPTION_REGEX = re.compile('^(.+)(\[autoexec\].+)$', re.M|re.DOTALL)
 
     def install_machine(self, machine: Machine) -> bool:
         success = super().install_machine(machine)
@@ -51,9 +54,23 @@ class PCSystem(BaseSystem):
         machine_dir = machine.resource.target_path.path
         config_path = machine_dir.joinpath('dosbox.conf')
 
+        with config_path.open('r') as config_file:
+            config_content = config_file.read()
+
+            # Split out the non-[autoexec] sections from the [autoexec] section
+            # 
+            # This is done because ConfigParser will fail on the non-standard
+            # content within the [autoexec] section.
+            # 
+            # [autoexec] will be added back to the config once the rest of the
+            # file has been processed
+            config_groups = re.search(self.AUTOEXEC_OPTION_REGEX, config_content)
+            non_autoexec_content = config_groups.group(1)
+            autoexec_content = config_groups.group(2)
+
         config = configparser.ConfigParser()
         config.optionxform = str
-        config.read(config_path)
+        config.read_string(non_autoexec_content)
         overwrite = False
 
         # Migrate predefined config to one suitable for the current emulator
@@ -73,12 +90,21 @@ class PCSystem(BaseSystem):
                 self.override_config(machine, config, overrides_path)
 
         # Overwrite the config if we made any changes
-        with config_path.open('r') as config_file:
+        with config_path.open('w') as config_file:
             config.write(config_file)
 
+            # Add back the [autoexec] section
+            config_file.write(autoexec_content)
+
     # Migrate the configuration based on the given migration file
-    def migrate_config(self, machine: Machine, config: configparser.ConfigParser, migration_path: str) -> None:
-        migration = json.loads(migration_path)
+
+    def migrate_config(self, machine: Machine, config: configparser.ConfigParser, migration_path: Path) -> None:
+        with migration_path.open() as migration_file:
+            migration = json.load(migration_file)
+
+        # Process translations first before removing options from the file
+        if 'translations' in migration:
+            self.translate_config(config, migration['translations'])
 
         if 'blocklist' in migration:
             self.filter_config(config, migration['blocklist'], True)
@@ -86,35 +112,43 @@ class PCSystem(BaseSystem):
         if 'allowlist' in migration:
             self.filter_config(config, migration['allowlist'], False)
 
-        if 'translations' in migration:
-            self.translate_config(config, migration['translations'])
-
     # Filters options from the given configuration
     def filter_config(self, config: configparser.ConfigParser, filters: dict, invert: bool) -> None:
         for section_name in config.sections():
             if section_name in filters:
                 section_filters = filters[section_name]
-                for option, value in config[section_name].items(raw=True):
+                for option, _ in config.items(section_name):
                     if (option in section_filters) == invert:
                         config.remove_option(section_name, option)
+
+            # Remove empty sections
+            if len(config[section_name]) == 0:
+                config.remove_section(section_name)
 
     # Translates from one option to another (when the actual section / option names have
     # changed from what's in the predefined config files)
     def translate_config(self, config: configparser.ConfigPraser, translations: dict) -> None:
         for section_name in translations:
-            if config.has_section(section):
+            if config.has_section(section_name):
                 section = config[section_name]
                 section_translations = translations[section_name]
 
-                for option, value in section.items(raw=True):
+                for option, _ in section.items():
                     if option in section_translations:
                         value = section.get(option, raw=True)
-                        new_value_template = section_translations[option]
+
+                        # Generate the new value
+                        new_section_name, new_option, new_value_template = section_translations[option]
                         new_value = eval(f"f'{new_value_template}'")
-                        config.set(section_name, option, new_value)
+
+                        # Ensure the section exists, otherwise `set` will fail
+                        if new_section_name not in config:
+                            config.add_section(new_section_name)
+
+                        config.set(new_section_name, new_option, new_value)
 
     # Override with configuration with Machine-specific settings
-    def override_config(self, machine: Machine, config: configparser.ConfigParser, overrides_path: str) -> None:
+    def override_config(self, machine: Machine, config: configparser.ConfigParser, overrides_path: Path) -> None:
         config.read(overrides_path)
 
     # Find two issues with paths:
