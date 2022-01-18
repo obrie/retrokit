21b045c330e3ac5a8d2b9243ae9b0bee1863604c
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-01-18 00:00:00 -0400

    Add a rudimentary vacuum CLI

---
+++ bin/vacuum.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+##############
+# Vacuum management
+# 
+# This will output `rm` commands to delete media files (roms, manuals, scraped media)
+# that it believes are no longer needed based on the current set of roms installed by
+# romkit.
+##############
+
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+. "$dir/common.sh"
+
+usage() {
+  echo "usage:"
+  echo " $0 <all|roms|manuals|media> <all|system>"
+  exit 1
+}
+
+vacuum_all() {
+  vacuum_roms
+  vacuum_manuals
+  vacuum_media
+}
+
+vacuum_roms() {
+  $bin_dir/setup.sh vacuum system-roms-download "$system"
+}
+
+vacuum_manuals() {
+  $bin_dir/setup.sh vacuum system-roms-manuals "$system"
+}
+
+vacuum_media() {
+  $bin_dir/setup.sh vacuum system-roms-scrape "$system"
+}
+
+main() {
+  local action=$1
+  local system=$2
+
+  if [ -z "$system" ] || [ "$system" == 'all' ]; then
+    while read system; do
+      "vacuum_$action" "$system" "${@:3}" | grep -E "^rm "
+    done < <(setting '.systems[]')
+  else
+    "vacuum_$action" "$system" "${@:3}" | grep -E "^rm "
+  fi
+}
+
+if [[ $# -lt 1 ]]; then
+  usage
+fi
+
+main "$@"
