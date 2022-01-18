c98bb65f98ffb4f4570efc9ba4b21602f571c299
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-01-18 00:00:00 -0400

    Fix iso_to_cso bugs

--- bin/romkit/resources/actions/iso_to_cso.py
+++ bin/romkit/resources/actions/iso_to_cso.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from romkit.resources.actions.base import BaseAction
 
 import subprocess
+import tempfile
+from pathlib import Path
 
 class IsoToCso(BaseAction):
     name = 'iso_to_cso'
@@ -10,7 +12,7 @@ class IsoToCso(BaseAction):
     def install(self, source, target, **kwargs):
         with tempfile.TemporaryDirectory() as tmp_dir:
             # Run maxcso
-            tmp_target = tmp_dir.joinpath('out.cso')
+            tmp_target = Path(tmp_dir).joinpath('out.cso')
             subprocess.run(['maxcso', '--block=16384', source.path, '-o', tmp_target], check=True)
             tmp_target.rename(target.path)
 
