d5270d5bc4752e0653651a65f7ade4f69d5c167d
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-01-19 00:00:00 -0400

    Support romset-specific filter sets

--- bin/romkit/models/romset.py
+++ bin/romkit/models/romset.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from romkit.auth import BaseAuth
 from romkit.discovery import BaseDiscovery
+from romkit.filters import FilterSet
 from romkit.models.machine import Machine
 from romkit.resources.resource import ResourceTemplate
 from romkit.util import Downloader
@@ -24,6 +25,7 @@ class ROMSet:
         discovery: Optional[dict] = None,
         datlist: Optional[List[str]] = None,
         downloads: Optional[dict] = None,
+        filters: Optional[dict]  = None,
         context: dict = {},
     ):
         self.system = system
@@ -32,6 +34,7 @@ class ROMSet:
         self.url = url
         self.emulator = emulator
         self.downloader = Downloader(auth=auth, **downloads)
+        self.filter_set = FilterSet.from_json(filters or {}, system.config, system.supported_filters)
 
         # Configure resources
         discovery = discovery and BaseDiscovery.from_json(discovery, downloader=self.downloader)
@@ -63,6 +66,7 @@ class ROMSet:
             auth=json.get('auth'),
             discovery=json.get('discovery'),
             datlist=json.get('datlist'),
+            filters=json.get('filters'),
             **kwargs,
         )
 
@@ -115,7 +119,8 @@ class ROMSet:
                 if Machine.is_installable(element):
                     machine = Machine.from_xml(self, element)
                     machine.custom_context = self.system.context_for(machine)
-                    yield machine
+                    if self.filter_set.allow(machine):
+                        yield machine
                 else:
                     logging.debug(f"[{element.get('name')}] Ignored (not installable)")
                 
