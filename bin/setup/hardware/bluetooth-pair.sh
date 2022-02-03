0bbf62243342f5f9aa3c6ca8992dde95fd0d5fd5
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-02-03 00:00:00 -0400

    Change bluetooth-pair to not do an unnecessary backup

--- bin/setup/hardware/bluetooth-pair.sh
+++ bin/setup/hardware/bluetooth-pair.sh
@@ -4,17 +4,11 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/../../common.sh"
 
 install() {
-  # Back up bluetooth settings
-  if [ ! -d '/var/lib/bluetooth.rk-src' ]; then
-    sudo cp -av /var/lib/bluetooth /var/lib/bluetooth.rk-src
-  fi
-
   sudo ~/RetroPie-Setup/retropie_packages.sh bluetooth gui
 }
 
 uninstall() {
-  sudo ~/RetroPie-Setup/retropie_packages.sh bluetooth gui
-  sudo rm -rfv /var/lib/bluetooth.rk-src
+  return
 }
 
 "${@}"
