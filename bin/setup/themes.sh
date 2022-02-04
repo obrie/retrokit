f4cc5130cb666fbd79e26c351892d27a5a57aefb
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-02-04 00:00:00 -0400

    themes setupscript cleanup

--- bin/setup/themes.sh
+++ bin/setup/themes.sh
@@ -9,7 +9,7 @@ install() {
   while IFS=$'\t' read -r name repo; do
     sudo "$HOME/RetroPie-Setup/retropie_packages.sh" esthemes install_theme "$name" "$repo"
     installed_themes["$name"]=1
-  done < <(setting '.themes.library[] | [.name, .repo] | @tsv')
+  done < <(_list_installed_themes)
 
   # Uninstall unused themes
   while read -r name; do
@@ -25,20 +25,30 @@ configure() {
   # Add theme overrides
   while read -r theme_path; do
     file_cp "$config_dir/themes/$theme_path" "/etc/emulationstation/themes/$theme_path" as_sudo=true envsubst=false
-  done < <(find "$config_dir/themes" -type f -printf "%P\n")
+  done < <(_list_theme_override_paths)
 }
 
 restore() {
   # Restore original theme
   while read -r theme_path; do
     restore_file "/etc/emulationstation/themes/$theme_path" as_sudo=true delete_src=true
-  done < <(find "$config_dir/themes" -type f -printf "%P\n")
+  done < <(_list_theme_override_paths)
+}
+
+# Lists themes configured to be installed
+_list_installed_themes() {
+  setting '.themes.library[] | [.name, .repo] | @tsv'
+}
+
+# List the theme paths that are being overridden
+_list_theme_override_paths() {
+  find "$config_dir/themes" -type f -printf "%P\n"
 }
 
 uninstall() {
   while IFS=$'\t' read -r name repo; do
     sudo "$HOME/RetroPie-Setup/retropie_packages.sh" esthemes uninstall_theme "$name" "$repo"
-  done < <(setting '.themes.library[] | [.name, .repo] | @tsv')
+  done < <(_list_installed_themes)
 }
 
 "${@}"
