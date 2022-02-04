b06694caf22bc697bd21a2789061bff644cc80f3
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-02-04 00:00:00 -0400

    Clean up boot setup script

--- bin/setup/boot.sh
+++ bin/setup/boot.sh
@@ -5,21 +5,29 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 configure() {
   __configure_bios
-  __configure_cmdline
+  __configure_kernel
 }
 
 __configure_bios() {
-  ini_merge "$config_dir/boot/config.txt" '/boot/config.txt' space_around_delimiters=false as_sudo=true
-
   # Note that we can't use crudini for dtoverlay additions because it doesn't
   # support repeating the same key multiple times in the same section
 
-  # Wifi configuration
+  ini_merge "$config_dir/boot/config.txt" '/boot/config.txt' space_around_delimiters=false as_sudo=true
+
+  __configure_bios_wifi
+  __configure_bios_ir
+  __configure_bios_case
+}
+
+# Wifi configuration
+__configure_bios_wifi() {
   if [ "$(setting '.hardware.wifi.enabled')" == 'false' ]; then
     echo 'dtoverlay=disable-wifi' | sudo tee -a /boot/config.txt
   fi
+}
 
-  # IR configuration
+# IR configuration
+__configure_bios_ir() {
   local ir_gpio_pin=$(setting '.hardware.ir.gpio_pin')
   local ir_keymap_path=$(setting '.hardware.ir.keymap')
   if [ -n "$ir_gpio_pin" ] || [ -n "$ir_keymap_path" ]; then
@@ -29,16 +37,18 @@ __configure_bios() {
 
     echo "dtoverlay=gpio-ir,gpio_pin=$ir_gpio_pin,rc-map-name=$rc_map_name" | sudo tee -a /boot/config.txt
   fi
+}
 
-  # Add case-specific boot options.  We do this here instead of the case setup
-  # in order to avoid multiple scripts modifying the /boot/config.txt file.
+# Add case-specific boot options.  We do this here instead of the case setup
+# in order to avoid multiple scripts modifying the /boot/config.txt file.
+__configure_bios_case() {
   local case=$(setting '.hardware.case.model')
-  if [ -f "$config_dir/boot/config-$case.txt" ]; then
-    cat "$config_dir/boot/config-$case.txt" | sudo tee -a /boot/config.txt >/dev/null
+  if [ -f "$config_dir/boot/config/$case.txt" ]; then
+    cat "$config_dir/boot/config/$case.txt" | sudo tee -a /boot/config.txt >/dev/null
   fi
 }
 
-__configure_cmdline() {
+__configure_kernel() {
   backup_and_restore '/boot/cmdline.txt' as_sudo=true
 
   while IFS=$'\t' read search_option replace_option; do
