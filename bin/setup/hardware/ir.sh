65fdc696a008fafd274b2c5baf286297dcf3baf5
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-02-04 00:00:00 -0400

    Clean up hardware/ir setupscript

--- bin/setup/hardware/ir.sh
+++ bin/setup/hardware/ir.sh
@@ -3,28 +3,26 @@
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 . "$dir/../../common.sh"
 
+source_keymap_path=$(setting '.hardware.ir.keymap')
+source_keymap_name=$(basename "$keymap_path")
+target_keymap_path="/etc/rc_keymaps/$keymap_name"
+keymap_config_path=/opt/retropie/configs/all/rc_keymap.cfg
+
 install() {
   sudo apt install -y ir-keytable
   configure
 }
 
 configure() {
-  local keymap_path=$(setting '.hardware.ir.keymap')
-  local keymap_name=$(basename "$keymap_path")
-  local target_path="/etc/rc_keymaps/$keymap_name"
-
   # Define a config file to be read by the autoconfig script when setting up a controller
-  local config_path='/opt/retropie/configs/all/rc_keymap.cfg'
-  rm -f "$config_path"
-  touch "$config_path"
-  crudini --set "$config_path" '' 'source_keymap_path' "$keymap_path"
-  crudini --set "$config_path" '' 'target_keymap_path' "$target_path"
+  rm -f "$keymap_config_path"
+  touch "$keymap_config_path"
+  crudini --set "$keymap_config_path" '' 'source_keymap_path' "$source_keymap_path"
+  crudini --set "$keymap_config_path" '' 'target_keymap_path' "$target_keymap_path"
 }
 
 uninstall() {
-  local keymap_path=$(setting '.hardware.ir.keymap')
-  local keymap_name=$(basename "$keymap_path")
-  sudo rm -fv "/etc/rc_keymaps/$keymap_name" /opt/retropie/configs/all/rc_keymap.cfg
+  sudo rm -fv "$target_keymap_path" "$keymap_config_path"
   sudo apt remove -y ir-keytable
 }
 
