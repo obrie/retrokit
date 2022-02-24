72c4946c6128e9bf83cf9d1c7a6edab131d98ecd
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-02-24 00:00:00 -0400

    Add romset sorter to romkit

---
+++ bin/romkit/sorters/romset.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from romkit.sorters.base import SubstringSorter
+
+# Sort on presence of a romset
+class RomsetSorter(SubstringSorter):
+    name = 'romsets'
+
+    def value(self, machine: Machine) -> str:
+        return machine.flags_str
