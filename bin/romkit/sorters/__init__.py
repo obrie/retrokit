40ce097fc838f61efd6fa453d0aa61cc45a8f90a
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-02-24 00:00:00 -0400

    Add romset sorter to romkit

--- bin/romkit/sorters/__init__.py
+++ bin/romkit/sorters/__init__.py
@@ -2,10 +2,12 @@ from romkit.sorters.sorter_set import SorterSet
 
 from romkit.sorters.description import FlagSorter, KeywordSorter
 from romkit.sorters.name import LengthSorter, NameSorter
+from romkit.sorters.romset import RomsetSorter
 
 __all_sorters__ = [
     FlagSorter,
     KeywordSorter,
     LengthSorter,
     NameSorter,
+    RomsetSorter,
 ]
