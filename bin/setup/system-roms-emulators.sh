47a298c52155b87f2ece4ee1da871c8965627467
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-03-01 00:00:00 -0400

    system-roms-download: Separate emulator configuration into a separately configurable setup script

---
+++ bin/setup/system-roms-emulators.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+. "$dir/system-common.sh"
+
+setup_module_id='system-roms-emulators'
+setup_module_desc='Manages emulator selections for individual ROMs'
+
+# Define emulators for games that don't use the default
+configure() {
+  local emulators_config_file='/opt/retropie/configs/all/emulators.cfg'
+  backup_file "$emulators_config_file"
+
+  # Load emulator data
+  load_emulator_data
+
+  # Identify new emulator selections
+  declare -A installed_keys
+  local selections_cfg=''
+  while IFS=$'\t' read -r rom_name source_emulator; do
+    local target_emulator=${emulators["$source_emulator/emulator"]:-$source_emulator}
+    local config_key=$(__clean_emulator_config_key "${system}_${rom_name}")
+
+    # Remove it from existing selections so we know what we should delete
+    # at the end of this
+    installed_keys["$config_key"]=1
+    selections_cfg+="$config_key = \"$target_emulator\"\n"
+  done < <(romkit_cache_list | jq -r 'select(.emulator) | [.name, .emulator] | @tsv')
+
+  # Add emulator selections for roms with an explicit one
+  echo 'Adding emulator selections...'
+  crudini --merge "$emulators_config_file" < <(echo -e "$selections_cfg")
+
+  # Remove emulator selections for roms without one
+  echo 'Removing unused emulator selections...'
+  while read -r config_key; do
+    [ "${installed_keys["$config_key"]}" ] || crudini --del "$emulators_config_file" '' "$config_key"
+  done < <(crudini --get "$emulators_config_file" '' | grep -E "^${system}_")
+}
+
+# Clean the configuration key used for defining ROM-specific emulator options
+# 
+# Implementation pulled from retropie
+__clean_emulator_config_key() {
+  local name="$1"
+  name="${name//\//_}"
+  name="${name//[^a-zA-Z0-9_\-]/}"
+  echo "$name"
+}
+
+restore() {
+  echo 'Removing emulator selections...'
+  sed -i "/^${system}_/d"
+}
+
+setup "$1" "${@:3}"
