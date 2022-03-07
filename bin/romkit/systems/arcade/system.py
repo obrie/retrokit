188b07ad97256850ec49fe50aa26351f20c6de3a
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-03-07 00:00:00 -0400

    arcade: Move emulator compatibility ratings to git to avoid an external dependency that can break installs

--- bin/romkit/systems/arcade/system.py
+++ bin/romkit/systems/arcade/system.py
@@ -1,11 +1,10 @@
 from romkit.systems import BaseSystem
 from romkit.metadata import EmulatorMetadata
-from romkit.systems.arcade.metadata import ArcadeEmulatorMetadata, GenreMetadata, LanguageMetadata, RatingMetadata
+from romkit.systems.arcade.metadata import GenreMetadata, LanguageMetadata, RatingMetadata
 
 class ArcadeSystem(BaseSystem):
     name = 'arcade'
-    supported_metadata = [cls for cls in BaseSystem.supported_metadata if cls != EmulatorMetadata] + [
-      ArcadeEmulatorMetadata,
+    supported_metadata = BaseSystem.supported_metadata + [
       GenreMetadata,
       LanguageMetadata,
       RatingMetadata,
