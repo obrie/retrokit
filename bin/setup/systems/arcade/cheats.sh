e7639004b511b055a0efb22b2265f382e08fe441
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-03-11 00:00:00 -0400

    systems/arcade/cheats: Refator and allow sources to be updated

--- bin/setup/systems/arcade/cheats.sh
+++ bin/setup/systems/arcade/cheats.sh
@@ -8,38 +8,63 @@ setup_module_id='systems/arcade/cheats'
 setup_module_desc='Cheats for Arcade systems'
 
 build() {
+  __build_fbneo
+  __build_mame2010
+  __build_mame2015
+  __build_mame2016
+  __build_advmame
+  __build_mame
+}
+
+__build_fbneo() {
   # Cheats: FBNeo
   if has_emulator 'lr-fbneo'; then
     mkdir -p "$HOME/RetroPie/BIOS/fbneo/cheats"
-    if [ ! -f "$system_tmp_dir/fbneo-cheats.zip" ]; then
-      download 'https://github.com/finalburnneo/FBNeo-cheats/archive/master.zip' "$system_tmp_dir/fbneo-cheats.zip"
-      unzip -j "$system_tmp_dir/fbneo-cheats.zip" 'FBNeo-cheats-master/cheats/*' -d "$HOME/RetroPie/BIOS/fbneo/cheats/"
+    local url='https://github.com/finalburnneo/FBNeo-cheats/archive/master.zip'
+
+    if [ -z "$(ls -A "$HOME/RetroPie/BIOS/fbneo/cheats/")" ] || [ "$FORCE_UPDATE" == 'true' ]; then
+      download "$url" "$tmp_ephemeral_dir/fbneo-cheats.zip"
+      unzip -jo "$tmp_ephemeral_dir/fbneo-cheats.zip" 'FBNeo-cheats-master/cheats/*' -d "$HOME/RetroPie/BIOS/fbneo/cheats/"
+    else
+      echo "Already downloaded $url"
     fi
   fi
+}
 
-  # Cheats: MAME (libretro)
+__build_mame2010() {
   if has_emulator 'lr-mame2010'; then
     download 'https://github.com/libretro/mame2010-libretro/raw/master/metadata/cheat.zip' "$HOME/RetroPie/BIOS/mame2010/cheat.zip"
   fi
+}
 
+__build_mame2015() {
   if has_emulator 'lr-mame2015'; then
     download 'https://github.com/libretro/mame2015-libretro/raw/master/metadata/cheat.7z' "$HOME/RetroPie/BIOS/mame2015/cheat.7z"
   fi
+}
 
+__build_mame2016() {
   if has_emulator 'lr-mame2016'; then
     download 'https://github.com/libretro/mame2016-libretro/raw/master/metadata/cheat.7z' "$HOME/RetroPie/BIOS/mame2016/cheat.7z"
   fi
+}
 
-  # Cheats: AdvMAME
+__build_advmame() {
   if has_emulator 'advmame'; then
     download 'https://github.com/amadvance/advancemame/raw/master/support/cheat.dat' "$HOME/RetroPie/BIOS/advmame/cheat.dat"
   fi
+}
 
+__build_mame() {
   # Cheats: MAME (Pugsy)
   if has_emulator 'lr-mame0222'; then
-    if [ ! -f "$system_tmp_dir/mame-cheats.zip" ]; then
-      download 'http://cheat.retrogames.com/download/cheat0221.zip' "$system_tmp_dir/mame-cheats.zip"
-      unzip -j "$system_tmp_dir/mame-cheats.zip" "cheat.7z" -d "$HOME/RetroPie/BIOS/mame/"
+    local url='http://cheat.retrogames.com/download/cheat0221.zip'
+
+    if [ ! -f "$HOME/RetroPie/BIOS/mame/cheat.7z" ] || [ "$FORCE_UPDATE" == 'true' ]; then
+      download "$url" "$tmp_ephemeral_dir/mame-cheats.zip"
+      unzip -jo "$tmp_ephemeral_dir/mame-cheats.zip" "cheat.7z" -d "$HOME/RetroPie/BIOS/mame/"
+    else
+      echo "Already downloaded $url"
     fi
   fi
 }
