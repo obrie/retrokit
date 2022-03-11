3f8ac149e80d58507a98f06caed02be60e6e1a37
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-03-11 00:00:00 -0400

    systems/videopac/samples: Refactor to only download relevant samples, allow for updating

--- bin/setup/systems/videopac/samples.sh
+++ bin/setup/systems/videopac/samples.sh
@@ -8,13 +8,37 @@ setup_module_id='system/videopac/samples'
 setup_module_desc='Voice Samples for Videopac/Odyssey'
 
 build() {
-  download 'http://o2em.sourceforge.net/files/o2mainsamp.zip' "$tmp_ephemeral_dir/o2mainsamp.zip"
-  download 'http://o2em.sourceforge.net/files/sidsamp.zip' "$tmp_ephemeral_dir/sidsamp.zip"
-
   mkdir -p "$HOME/RetroPie/BIOS/voice"
 
-  unzip -j "$tmp_ephemeral_dir/o2mainsamp.zip" -d "$HOME/RetroPie/BIOS/voice/"
-  unzip -j "$tmp_ephemeral_dir/sidsamp.zip" -d "$HOME/RetroPie/BIOS/voice/"
+  __build_common_samples
+  __build_sid_the_spellbinder_samples
+}
+
+# Samples common to multiple games
+__build_common_samples() {
+  local url='http://o2em.sourceforge.net/files/o2mainsamp.zip'
+
+  if [ ! -f "$HOME/RetroPie/BIOS/voice/E4BB.WAV" ] || [ "$FORCE_UPDATE" == 'true' ]; then
+    download "$url" "$tmp_ephemeral_dir/o2mainsamp.zip"
+    unzip -jo "$tmp_ephemeral_dir/o2mainsamp.zip" -d "$HOME/RetroPie/BIOS/voice/"
+  else
+    echo "Already downloaded $url"
+  fi
+}
+
+# Samples specific to Sid the Spellbinder
+__build_sid_the_spellbinder_samples() {
+  local machine=$(romkit_cache_list | jq 'select(.title == "Sid the Spellbinder")')
+  local url='http://o2em.sourceforge.net/files/sidsamp.zip'
+
+  if [ -n "$machine" ]; then
+    if [ ! -f "$HOME/RetroPie/BIOS/voice/EE80.WAV" ] || [ "$FORCE_UPDATE" == 'true' ]; then
+      download "$url" "$tmp_ephemeral_dir/sidsamp.zip"
+      unzip -jo "$tmp_ephemeral_dir/sidsamp.zip" -d "$HOME/RetroPie/BIOS/voice/"
+    else
+      echo "Already downloaded $url"
+    fi
+  fi
 }
 
 remove() {
