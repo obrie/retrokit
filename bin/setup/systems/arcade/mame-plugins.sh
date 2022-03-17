86fd1db94ffd7d30b9be0ac75da85fec4c41b6b2
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-03-17 00:00:00 -0400

    mame: Cleanup new dat scripts

--- bin/setup/systems/arcade/mame-plugins.sh
+++ bin/setup/systems/arcade/mame-plugins.sh
@@ -13,18 +13,22 @@ build() {
 }
 
 __build_mame2016() {
-  if has_emulator 'lr-mame2016' && [ ! -f "$HOME/RetroPie/BIOS/mame2016/plugins/boot.lua" ]; then
-    git clone --depth 1 https://github.com/libretro/mame2016-libretro "$tmp_ephemeral_dir/mame2016-libretro"
-    rm -rf "$HOME/RetroPie/BIOS/mame2016/plugins"
-    cp -R "$tmp_ephemeral_dir/mame2016-libretro/plugins/" "$HOME/RetroPie/BIOS/mame2016/"
+  if has_emulator 'lr-mame2016'; then
+    if [ ! -f "$HOME/RetroPie/BIOS/mame2016/plugins/boot.lua" ] || [ "$FORCE_UPDATE" == 'true' ]; then
+      git clone --depth 1 https://github.com/libretro/mame2016-libretro "$tmp_ephemeral_dir/mame2016-libretro"
+      rm -rf "$HOME/RetroPie/BIOS/mame2016/plugins"
+      cp -R "$tmp_ephemeral_dir/mame2016-libretro/plugins/" "$HOME/RetroPie/BIOS/mame2016/"
+    fi
   fi
 }
 
 __build_mame() {
-  if has_emulator 'lr-mame' && [ ! -f "$HOME/RetroPie/BIOS/mame/plugins/boot.lua" ]; then
-    git clone -b lrmame0222 --depth 1 https://github.com/libretro/mame.git "$tmp_ephemeral_dir/mame-libretro"
-    rm -rf "$HOME/RetroPie/BIOS/mame/plugins"
-    cp -R "$tmp_ephemeral_dir/mame-libretro/plugins/" "$HOME/RetroPie/BIOS/mame/"
+  if has_emulator 'lr-mame'; then
+    if [ ! -f "$HOME/RetroPie/BIOS/mame/plugins/boot.lua" ] || [ "$FORCE_UPDATE" == 'true' ]; then
+      git clone -b lrmame0222 --depth 1 https://github.com/libretro/mame.git "$tmp_ephemeral_dir/mame-libretro"
+      rm -rf "$HOME/RetroPie/BIOS/mame/plugins"
+      cp -R "$tmp_ephemeral_dir/mame-libretro/plugins/" "$HOME/RetroPie/BIOS/mame/"
+    fi
   fi
 }
 
