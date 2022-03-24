41e5d46be31e6755973cd7a55e30fb0f23a9051b
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-03-24 00:00:00 -0400

    systems/n64/config: Use mupen64plus's GLideN64.custom.ini as our backup source instead of managing a separate file

--- bin/setup/systems/n64/config.sh
+++ bin/setup/systems/n64/config.sh
@@ -8,13 +8,19 @@ setup_module_id='system/n64/config'
 setup_module_desc='N64 emulator configuration'
 
 configure() {
-  ini_merge '{system_config_dir}/GLideN64.custom.ini' "$retropie_system_config_dir/GLideN64.custom.ini"
+  __restore_mupen64plus_gliden64
+  ini_merge '{system_config_dir}/GLideN64.custom.ini' "$retropie_system_config_dir/GLideN64.custom.ini" backup=false
   ini_merge '{system_config_dir}/mupen64plus.cfg' "$retropie_system_config_dir/mupen64plus.cfg"
 }
 
 restore() {
   restore_file "$retropie_system_config_dir/mupen64plus.cfg" delete_src=true
-  restore_file "$retropie_system_config_dir/GLideN64.custom.ini" delete_src=true
+  __restore_mupen64plus_gliden64
+}
+
+__restore_mupen64plus_gliden64() {
+  # This file comes directly from the install, so we always use it as the base
+  cp /opt/retropie/emulators/mupen64plus/share/mupen64plus/GLideN64.custom.ini "$retropie_system_config_dir/GLideN64.custom.ini"
 }
 
 setup "${@}"
