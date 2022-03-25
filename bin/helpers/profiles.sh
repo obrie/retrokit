5f1479cda5e3f903641dfe96bb604b5a4dab1f25
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-03-25 00:00:00 -0400

    helpers/profiles: Improve execution performance

--- bin/helpers/profiles.sh
+++ bin/helpers/profiles.sh
@@ -2,6 +2,9 @@
 # Profile config helpers
 ##############
 
+# Read list of profiles
+IFS=', ' read -r -a profiles <<< "$PROFILES"
+
 # Determines whether any path exists from the given template.
 # 
 # See each_path for more information.
@@ -28,15 +31,15 @@ each_path() {
 
   # Determine which directory we're dealing with
   local template_name
-  if [[ "$path_template" == *{config_dir}* ]]; then
+  if [[ "$path_template" == {config_dir}* ]]; then
     template_name='config_dir'
     default_load_path='../config'
     sub_dir=''
-  elif [[ "$path_template" == *{system_config_dir}* ]]; then
+  elif [[ "$path_template" == {system_config_dir}* ]]; then
     template_name='system_config_dir'
     default_load_path='../config'
     sub_dir="systems/$system"
-  elif [[ "$path_template" == *{bin_dir}* ]]; then
+  elif [[ "$path_template" == {bin_dir}* ]]; then
     template_name='bin_dir'
     default_load_path='..'
     sub_dir='bin'
@@ -44,7 +47,7 @@ each_path() {
 
   if [ -n "$template_name" ]; then
     # Find matching paths within each profile
-    while read profile; do
+    for profile in "$default_load_path" "${profiles[@]}"; do
       # Make sure we're dealing with a valid profile
       local profile_dir="$profiles_dir/$profile"
       if [ ! -d "$profile_dir" ]; then
@@ -63,7 +66,7 @@ each_path() {
         local full_path=$(realpath "$rendered_path")
         process_path "$full_path" "${@:2}"
       fi
-    done < <(echo "$default_load_path,$PROFILES" | tr ',' $'\n' | grep .)
+    done
   elif [ -f "$path_template" ]; then
     # No template detected, but path exists
     process_path "${@}"
