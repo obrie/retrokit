24d2efc70fe81d313f5529076f9ffd275cfe19a3
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-03-25 00:00:00 -0400

    ports: Decouple package installation from game file installation so we can better control when this happens in the setup process
    Also properly support restore of game files

---
+++ bin/setup/systems/ports/gamefiles.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+system='ports'
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+. "$dir/../../system-common.sh"
+. "$dir/helpers.sh"
+
+setup_module_id='system/ports/gamefiles'
+setup_module_desc='Port file installation, using files from the system-roms-download setup script'
+
+configure() {
+  # Link over any optional game files
+  while IFS=$'\t' read -r port_name target_name source_path; do
+    file_ln "$source_path" "$HOME/RetroPie/roms/ports/$port_name/$target_name"
+  done < <(__list_gamefiles)
+}
+
+restore() {
+  while IFS=$'\t' read -r port_name target_name source_path; do
+    restore "$HOME/RetroPie/roms/ports/$port_name/$target_name"
+  done < <(__list_gamefiles)
+}
+
+__list_gamefiles() {
+  port_setting 'to_entries[] | .key as $name | .value | select(.files) | .files | to_entries[] | [$name, .key, .value] | @tsv'
+}
+
+setup "${@}"
