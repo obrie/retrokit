9e152e207a9aaf00a2653f0b3d9ef7c7234c374e
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-04-11 00:00:00 -0400

    docs: Rename controls_file to doc_data_file

--- bin/setup/system-roms-docs.sh
+++ bin/setup/system-roms-docs.sh
@@ -15,15 +15,15 @@ build() {
   local retroarch_config_dir=$(get_retroarch_path 'rgui_config_directory')
 
   # Start with a base controls file
-  local base_controls_file=$controls_file
-  echo '{}' > "$base_controls_file"
+  local base_doc_data_file=$doc_data_file
+  echo '{}' > "$base_doc_data_file"
   __add_hrefs
   __add_system_theme
   __add_keyboard_controls
   __add_hotkey_controls
 
   # Redefine the controls file for ROMs
-  controls_file="$tmp_ephemeral_dir/rom_controls.json"
+  doc_data_file="$tmp_ephemeral_dir/doc-rom.json"
 
   while IFS='»' read name parent_name emulator; do
     emulator=${emulator:-default}
@@ -45,7 +45,7 @@ build() {
     mkdir -p "$(dirname "$output_path")"
 
     # Build the PDF
-    cp "$base_controls_file" "$controls_file"
+    cp "$base_doc_data_file" "$doc_data_file"
     __add_system_extensions "$core_options_file" "$name" "$parent_name"
     __build_pdf "$output_path"
   done < <(romkit_cache_list | jq -r '[.name, .parent.name, .emulator] | join("»")')
