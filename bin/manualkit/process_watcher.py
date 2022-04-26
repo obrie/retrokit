024ee1bb4e3bc37c664d4e853e41c30ff44a7285
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-04-26 00:00:00 -0400

    manualkit: Fix various edge cases with ProcessWatcher * Allow watching to be enabled/disabled via the conf * Allow suspend feature to be enabled/disabled via the conf * Ensure that when we resume, we're resuming whatever process we previously suspended * Ensure that the suspended process is still running when we resume * When suspending, ensure that any previously suspended process has been resumed

--- bin/manualkit/process_watcher.py
+++ bin/manualkit/process_watcher.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 import psutil
 import signal
 import threading
@@ -9,11 +10,17 @@ from typing import Optional
 
 # Provides utilities for interacting with another process
 class ProcessWatcher():
-    _instance = None
-
-    def __init__(self, pid: int, callback: Callable) -> None:
+    def __init__(self,
+        pid: int,
+        callback: Callable,
+        enabled: bool = True,
+        suspend: bool = True,
+    ) -> None:
         self.parent_process = psutil.Process(pid)
         self.callback = callback
+        self.enabled = bool(enabled)
+        self.suspend_enabled = bool(suspend)
+        self.suspended_process = None
 
     # Looks up the last process in the tree -- this is the one we want
     # to interactive with
@@ -31,6 +38,9 @@ class ProcessWatcher():
     # Starts tracking the emulator process.  If that process terminates, then the given
     # callback will be invoked.
     def track(self) -> None:
+        if not self.enabled:
+            return
+
         self.running = True
 
         thread = threading.Thread(target=self._track_process, args=[self.callback])
@@ -44,15 +54,27 @@ class ProcessWatcher():
     # Suspends the emulator so that it doesn't continue to run while other
     # information is on-screen
     def suspend(self) -> None:
+        if not self.suspend_enabled:
+            return
+
         process = self.process
         if process:
-            process.send_signal(signal.SIGSTOP)
+            # Resume any previously suspended process
+            self.resume()
+
+            logging.debug(f'Suspending {process}')
+            self.suspended_process = process
+            self.suspended_process.send_signal(signal.SIGSTOP)
 
     # Resumes the emulator so the user can continue to play
     def resume(self) -> None:
-        process = self.process
-        if process:
-            process.send_signal(signal.SIGCONT)
+        if not self.suspend_enabled:
+            return
+
+        if self.suspended_process and self.suspended_process.is_running():
+            logging.debug(f'Resuming {self.suspended_process}')
+            self.suspended_process.send_signal(signal.SIGCONT)
+            self.suspended_process = None
 
     # Invokes the given callback when the emulator terminates
     def _track_process(self, callback: Callable) -> None:
