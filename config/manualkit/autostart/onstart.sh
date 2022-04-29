1be61065a6f048f3ecf6b70b826772ba15978780
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-04-29 00:00:00 -0400

    manualkit: Output logs somewhere that can be inspect

--- config/manualkit/autostart/onstart.sh
+++ config/manualkit/autostart/onstart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo python3 /opt/retropie/supplementary/manualkit/cli.py /opt/retropie/configs/all/manualkit.conf --server --profile frontend --track-pid $PPID >/dev/null 2>&1 &
+sudo python3 /opt/retropie/supplementary/manualkit/cli.py /opt/retropie/configs/all/manualkit.conf --server --profile frontend --track-pid $PPID > $HOME/.emulationstation/manualkit_log.txt 2>&1 &
