de504a31ec443e6bca189be6a8f6acd0744a3021
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-05-01 00:00:00 -0400

    manualkit: Appropriate fix to unescape game-start rom paths

--- config/manualkit/emulationstation-scripts/game-start.sh
+++ config/manualkit/emulationstation-scripts/game-start.sh
@@ -3,7 +3,7 @@
 # Determine ROM name
 # NOTE: The ROM path is escaped already by EmulationStation.  This is the only
 # script event in which the path is escaped.
-rom_path=$1
+eval rom_path=$1
 rom_filename=${rom_path##*/}
 rom_name=${rom_filename%.*}
 
@@ -13,11 +13,11 @@ system_relative_dir=${rom_path/"$HOME/RetroPie/roms/"/}
 system=${system_relative_dir%/*}
 
 media_dir="$HOME/.emulationstation/downloaded_media/$system"
-rom_manual_path="$media_dir/manuals/"$rom_name.pdf
+rom_manual_path="$media_dir/manuals/$rom_name.pdf"
 
 # Determine which reference guide to try to show
 if [ -f "$media_dir/docs/$rom_name.pdf" ]; then
-  rom_reference_path="$media_dir/docs/"$rom_name.pdf
+  rom_reference_path="$media_dir/docs/$rom_name.pdf"
 else
   rom_reference_path="$media_dir/docs/default.pdf"
 fi
