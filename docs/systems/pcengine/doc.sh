f092e7de975798a3deb4892e4af66e34a5f1ea52
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-05-18 00:00:00 -0400

    docs: Improvements to reference documentation across a variety of systems

--- docs/systems/pcengine/doc.sh
+++ docs/systems/pcengine/doc.sh
@@ -6,8 +6,8 @@ __add_system_extensions() {
   local edit_args=()
 
   # Turbo controls
-  local turbo_enabled=$(crudini --get "$core_options_file" '' fceumm_turbo_enable 2>/dev/null | tr -d '"')
-  if [ -n "$turbo_enabled" ] && [ "$turbo_enabled" != 'None' ]; then
+  local turbo_enabled=$(crudini --get "$core_options_file" '' pce_Turbo_Toggling 2>/dev/null | tr -d '"')
+  if [ "$turbo_enabled" == 'enabled' ]; then
     edit_args+=(
       '.controls.retropad."x"' 'Turbo A'
       '.controls.retropad."y"' 'Turbo B'
