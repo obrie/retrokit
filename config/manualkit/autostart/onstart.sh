6eb6c152eaacdcdd4d89b4d347f867b204d03bdf
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-05-18 00:00:00 -0400

    manualkit: Ensure any existing manualkit processes are terminated on autostart

--- config/manualkit/autostart/onstart.sh
+++ config/manualkit/autostart/onstart.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Ensure any previous instance of manualkit has been terminated
+sudo pkill -f manualkit/cli.py
+
 sudo python3 /opt/retropie/supplementary/manualkit/cli.py /opt/retropie/configs/all/manualkit.conf --server --profile frontend --track-pid $PPID > $HOME/.emulationstation/manualkit_log.txt 2>&1 &
