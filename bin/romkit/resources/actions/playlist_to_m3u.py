f2ef9648d29fb093286e81b0d4cc06c4df6268e9
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-05-19 00:00:00 -0400

    segacd: Fix playlists not working since the paths *must* be relative

--- bin/romkit/resources/actions/playlist_to_m3u.py
+++ bin/romkit/resources/actions/playlist_to_m3u.py
@@ -8,11 +8,16 @@ class PlaylistToM3U(BaseAction):
     name = 'playlist_to_m3u'
 
     def install(self, source: ResourcePath, target: ResourcePath, **kwargs) -> None:
+        if self.config.get('relative_to'):
+            source_path = source.path.relative_to(self.config.get('relative_to'))
+        else:
+            source_path = source.path
+
         with target.path.open('a+') as file:
             # Read/sort the paths
             file.seek(0)
             paths = file.readlines()
-            paths.append(f'{unquote(str(source.path))}\n')
+            paths.append(f'{unquote(str(source_path))}\n')
             paths.sort()
 
             # Write the new path list
