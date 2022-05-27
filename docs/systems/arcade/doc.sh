576c4765f0a5fb0e494cb949b90bfeacb0f94b52
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-05-27 00:00:00 -0400

    arcade: Rewrite control mapping logic to account for game-specific and emulator-specific layouts

--- docs/systems/arcade/doc.sh
+++ docs/systems/arcade/doc.sh
@@ -21,10 +21,7 @@ __add_system_extensions() {
   local core_options_path=$1
   local name=$2
   local parent_name=$3
-
-  # Look up the layout
-  local layout_button_index=($(jq -r '.controls .layout .ids[]' "$(first_path '{system_docs_dir}/doc.json')"))
-  local layout_button_names=($(jq -r '.controls .layout .buttons[]' "$(first_path '{system_docs_dir}/doc.json')"))
+  local emulator=$4
 
   # Read the button names for this game or use system defaults
   local button_actions=()
@@ -36,17 +33,40 @@ __add_system_extensions() {
     button_class='button-enabled'
   fi
 
+  # Look up the control panel layout
+  local control_panel_layout=$(jq -r '.controls .layout .panel' "$(first_path '{system_docs_dir}/doc.json')")
+
+  # Look up the button mapping layout
+  local button_mappings=$(jq -r "(.controls .layout .roms | to_entries[] | select(.value | index(\"${parent_name:-$name}\")) | .key) // (.controls .layout .emulators | to_entries[] | select(.value | index(\"$emulator\")) | .key) // (.controls .layout .emulators | to_entries[] | select(.value | index(\"default\")) | .key)" "$(first_path '{system_docs_dir}/doc.json')")
+
+  # Map button names to the corresponding actions based on their mapping index
+  # (i.e. the index of each button within $button_mappings)
+  # 
+  # The controls.csv maps BUTTON_1, BUTTON_2, etc. and the rom/emulator layout
+  # maps the panel button name to the action by index.
+  declare -A button_name_actions
+  for (( i=0; i<${#button_mappings}; i++ )); do
+    local button_name=${button_mappings:$i:1}
+    local button_action=${button_actions[$i]}
+
+    button_name_actions[$button_name]=$button_action
+  done
+
   # Build top row
   local html=""
   for column in $(seq 1 3); do
-    local button_index=${layout_button_index[column-1]}
-    html="${html}$(__create_button 'top' $column "${layout_button_names[column-1]}" "${button_actions[button_index-1]}" "$button_class")"
+    local control_panel_name=${control_panel_layout:column-1:1}
+    local button_action=${button_name_actions[$control_panel_name]}
+
+    html="${html}$(__create_button 'top' $column "$control_panel_name" "$button_action" "$button_class")"
   done
 
   # Build bottom row
   for column in $(seq 1 3); do
-    local button_index=${layout_button_index[column+2]}
-    html="${html}$(__create_button 'bottom' $column "${layout_button_names[column+2]}" "${button_actions[button_index-1]}" "$button_class")"
+    local control_panel_name=${control_panel_layout:column+2:1}
+    local button_action=${button_name_actions[$control_panel_name]}
+
+    html="${html}$(__create_button 'bottom' $column "$control_panel_name" "$button_action" "$button_class")"
   done
 
   html="<ol id=\"controller-retropad-buttons\">$html</ol>"
