5c7587eb1f097b9964a428f9532d2b03ee575500
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-05-31 00:00:00 -0400

    Support changing the login/password via the script

--- bin/setup/ssh.sh
+++ bin/setup/ssh.sh
@@ -7,8 +7,19 @@ setup_module_id='ssh'
 setup_module_desc='Enables SSH authentication'
 
 configure() {
+  __configure_service
+  __configure_authentication
+}
+
+__configure_service() {
   sudo systemctl enable ssh
   sudo systemctl start ssh
+}
+
+__configure_authentication() {
+  if [ -n "$LOGIN_PASSWORD" ] && [ -n "$LOGIN_USER" ]; then
+    echo "$LOGIN_USER:$LOGIN_PASSWORD" | sudo chpasswd
+  fi
 }
 
 setup "${@}"
