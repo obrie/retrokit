f8d43d348fc57b0cba40f4c20a2ed8d444fa9f24
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-06-11 00:00:00 -0400

    docs: Ensure appropriate fonts are installed on the system

--- bin/setup/docs.sh
+++ bin/setup/docs.sh
@@ -7,7 +7,7 @@ setup_module_id='docs'
 setup_module_desc='Documentation builder for Retrokit'
 
 depends() {
-  sudo apt-get install -y chromium
+  sudo apt-get install -y chromium fonts-roboto
 
   sudo pip3 install jinja2-cli
 }
