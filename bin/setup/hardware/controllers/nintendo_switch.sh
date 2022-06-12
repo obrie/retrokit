0d0eda92ee8fef50f8bebfae38e658bebeb5db38
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-06-12 00:00:00 -0400

    hardware/controllers/nintendo_switch: Add joycond installation

--- bin/setup/hardware/controllers/nintendo_switch.sh
+++ bin/setup/hardware/controllers/nintendo_switch.sh
@@ -8,13 +8,21 @@ setup_module_desc='Nintendo switch controller setup and configuration'
 
 module_version=3.2
 
+deps() {
+  sudo apt-get install -y libevdev-dev
+}
+
 build() {
+  build_kernel_module
+  build_joycond
+}
+
+build_kernel_module() {
   if [ -d /var/lib/dkms/nintendo/$module_version ]; then
     return
   fi
 
   # Check out
-  rm -rf "$tmp_ephemeral_dir/dkms-hid-nintendo"
   git clone --depth 1 https://github.com/nicman23/dkms-hid-nintendo "$tmp_ephemeral_dir/dkms-hid-nintendo"
   pushd "$tmp_ephemeral_dir/dkms-hid-nintendo"
 
@@ -23,9 +31,37 @@ build() {
   sudo dkms install nintendo -v $module_version
 }
 
+build_joycond() {
+  if [ -f /usr/bin/joycond ] && [ "$FORCE_UPDATE" != 'true' ]; then
+    return
+  fi
+
+  git clone --depth 1 https://github.com/DanielOgorchock/joycond "$tmp_ephemeral_dir/joycond"
+  pushd "$tmp_ephemeral_dir/joycond"
+
+  cmake .
+  sudo make install
+  sudo systemctl enable --now joycond
+}
+
+restore() {
+  sudo systemctl disable --now joycond
+}
+
 remove() {
+  # Remove all remnants of joycond
+  sudo rm -rfv \
+    /usr/bin/joycond \
+    /lib/udev/rules.d/89-joycond.rules \
+    /lib/udev/rules.d/72-joycond.rules \
+    /etc/systemd/system/joycond.service \
+    /etc/modules-load.d/joycond.conf \
+
+  # Remove the nintendo kernel module
   sudo dkms uninstall nintendo -v $module_version
   sudo dkms remove nintendo/$module_version --all
+
+  sudo apt-get remove -y libevdev-dev
 }
 
 setup "${@}"
