a9ac3c7308e25c8a3648b85ffe8fc116042c19ef
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-06-13 00:00:00 -0400

    ssh: Add support for configuring authorized/private keys

--- bin/setup/ssh.sh
+++ bin/setup/ssh.sh
@@ -9,6 +9,8 @@ setup_module_desc='Enables SSH authentication'
 configure() {
   __configure_service
   __configure_authentication
+  __configure_authorized_keys
+  __configure_private_keys
 }
 
 __configure_service() {
@@ -20,6 +22,31 @@ __configure_authentication() {
   if [ -n "$LOGIN_PASSWORD" ] && [ -n "$LOGIN_USER" ]; then
     echo "$LOGIN_USER:$LOGIN_PASSWORD" | sudo chpasswd
   fi
+}
+
+__configure_authorized_keys() {
+  file_cp '{config_dir}/ssh/authorized_keys' "$HOME/.ssh/authorized_keys"
+}
+
+__configure_private_keys() {
+  while read source_key_path; do
+    local keyname=$(basename "$source_key_path")
+    local target_key_path="$HOME/.ssh/$keyname"
+
+    file_cp "$source_key_path" "$target_key_path"
+    chmod 600 "$target_key_path"
+  done < <(each_path '{config_dir}/ssh' find '{}' -name 'id_rsa*')
+}
+
+restore() {
+  restore_file "$HOME/.ssh/authorized_keys" delete_src=true
+
+  while read key_path; do
+    restore_file "$key_path" delete_src=true
+  done < <(ls "$HOME/.ssh/id_rsa-"* | grep -Ev 'rk-src')
+
+  sudo systemctl stop ssh
+  sudo systemctl disable ssh
 }
 
 setup "${@}"
