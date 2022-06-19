3310d707f87ee3b75fb483dab90cb796d74bb50a
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-06-19 00:00:00 -0400

    cpu: Add support for being able to configure the cpu governor on boot

---
+++ bin/setup/hardware/cpu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+. "$dir/../../common.sh"
+
+setup_module_id='hardware/cpu'
+setup_module_desc='CPU configuration to improve performance'
+
+configure() {
+  sudo systemctl disable raspi-config
+  sudo apt-get install -y cpufrequtils
+  file_cp '{config_dir}/cpu/cpufrequtils' '/etc/default/cpufrequtils' as_sudo=true
+}
+
+restore() {
+  restore_file '/etc/default/cpufrequtils' as_sudo=true delete_src=true
+  sudo apt-get remove -y cpufrequtils
+  sudo systemctl enable raspi-config
+}
+
+setup "${@}"
