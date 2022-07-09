3df79e2676f01cd1579a564c69214194286f7fe4
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-07-09 00:00:00 -0400

    xpad / xpadneo: Support modprobe conf overrides

--- bin/setup/hardware/controllers/xpad-bluetooth.sh
+++ bin/setup/hardware/controllers/xpad-bluetooth.sh
@@ -6,8 +6,19 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 setup_module_id='hardware/controllers/xpad-bluetooth'
 setup_module_desc='Xbox bluetooth controller setup and configuration'
 
+xpadneo_conf_file='/etc/modprobe.d/99-xpadneo-bluetooth-overrides.conf'
+
 build() {
   install_retropie_package 'supplementary' 'xpadneo-plus' 'source'
+}
+
+configure() {
+  backup_and_restore "$xpadneo_conf_file" as_sudo=true
+  each_path '{config_dir}/controllers/xpad/xpadneo.conf' cat '{}' | sudo tee -a "$xpadneo_conf_file" >/dev/null
+}
+
+restore() {
+  restore_file "$xpadneo_conf_file" as_sudo=true
 }
 
 remove() {
