dd93fec8f24fa5143eb8f9837968696fbc127463
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-07-31 00:00:00 -0400

    psx: Improve processing of Duckstation metadata so that we gather data from multiple regions to identify the full scope of metadata for a single region

--- bin/romkit/systems/psx/metadata.py
+++ bin/romkit/systems/psx/metadata.py
@@ -4,6 +4,7 @@ from romkit.metadata.external import ExternalMetadata
 from romkit.models.machine import Machine
 
 import json
+import re
 from pathlib import Path
 
 # Game metadata managed by Duckstation
@@ -12,6 +13,8 @@ from pathlib import Path
 class DuckstationMetadata(ExternalMetadata):
     name = 'duckstation_data'
 
+    GENRE_REPLACE_REGEX = re.compile(r'\.+$')
+
     def load(self) -> None:
         self.metadata = {}
 
@@ -19,32 +22,62 @@ class DuckstationMetadata(ExternalMetadata):
             data = json.load(file)
             for game in data:
                 if 'genre' in game or 'language' in game or 'maxPlayers' in game:
-                    self.set_data(game['name'], {
-                        'genre': game.get('genre'),
-                        'language': game.get('language'),
-                        'players': game.get('maxPlayers'),
-                    })
+                    name = game['name']
+                    title = Machine.title_from(name)
+
+                    # Pre-existing shared metadata betweeen regional games
+                    shared_metadata = self.data.get(title) or {}
+
+                    language = game.get('language')
+                    players = game.get('maxPlayers')
+                    genre = game.get('genre')
+
+                    # Update players / genre shared metadata between regional games
+                    if players and (not shared_metadata.get('players') or shared_metadata['players'] < players):
+                        shared_metadata['players'] = players
+
+                    if genre and (not shared_metadata.get('genre')):
+                        genre = self.GENRE_REPLACE_REGEX.sub('', genre)
+                        shared_metadata['genre'] = genre
+
+                    # Region-specific metadata
+                    game_metadata = {}
+                    if language:
+                        game_metadata['language'] = language
+
+                    self.set_data(name, game_metadata)
+                    self.set_data(title, shared_metadata)
 
     def update(self, machine: Machine) -> None:
-        machine_data = self.data.get(Machine.normalize(machine.name))
+        # Prioritize (lowest to highest):
+        # * Parent Title
+        # * Parent Name
+        # * Title
+        # * Name
+        data = {
+            **self.data.get(Machine.normalize(machine.title), {}),
+            **self.data.get(Machine.normalize(machine.name), {}),
+        }
         if machine.parent_name:
-            parent_data = self.data.get(Machine.normalize(machine.parent_name))
-        else:
-            parent_data = None
+            data = {
+                **self.data.get(Machine.normalize(machine.parent_title), {}),
+                **self.data.get(Machine.normalize(machine.parent_name), {}),
+                **data,
+            }
 
         # Genre
-        genre = machine_data and machine_data['genre'] or parent_data and parent_data['genre']
+        genre = data.get('genre')
         if genre:
             # Prefer Duckstation genres over scraped genres
             machine.genres.clear()
             machine.genres.add(genre)
 
         # Language
-        language = machine_data and machine_data['language'] or parent_data and parent_data['language']
+        language = data.get('language')
         if language:
             machine.languages.add(language)
 
         # Num. Players (override only if Duckstation thinks more are supported)
-        players = machine_data and machine_data['players'] or parent_data and parent_data['players']
+        players = data.get('players')
         if players and (not machine.players or machine.players < players):
             machine.players = players
