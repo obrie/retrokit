b103adcd4db98107319b8cc2f2942a707a2f7153
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-08-26 00:00:00 -0400

    autoport: Fix detection of empty overrides

--- config/autoport/runcommand/onlaunch.sh
+++ config/autoport/runcommand/onlaunch.sh
@@ -72,7 +72,7 @@ __find_setting() {
   fi
 
   # Find the associated key within that section
-  if echo "$section" | grep -Eq "^[ \t]*$key[ \t]*"; then
+  if echo "$section" | grep -Eq "^[ \t]*$key[ \t]*="; then
     echo "$section" | sed -n "s/^[ \t]*$key[ \t]*=[ \t]*\"*\([^\"\r]*\)\"*.*/\1/p" | tail -n 1
   else
     return 1
