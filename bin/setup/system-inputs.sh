e457982496ed3ea4005611b8a05b18d7f7667998
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-08-27 00:00:00 -0400

    system-inputs: Fix autoport.cfg files not getting removed when they no longer exist

--- bin/setup/system-inputs.sh
+++ bin/setup/system-inputs.sh
@@ -7,6 +7,7 @@ setup_module_id='system-inputs'
 setup_module_desc='System-specific automatic port selection using autoport'
 
 configure() {
+  restore
   ini_merge '{system_config_dir}/autoport.cfg' "$retropie_system_config_dir/autoport.cfg" backup=false overwrite=true
 }
 
