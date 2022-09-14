111f491e1619912f21c7fd4670cd0dd8181e5656
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-09-14 00:00:00 -0400

    arcade: Replace lr-mame2016 with lr-mame2016-lightgun until https://github.com/libretro/mame2016-libretro/pull/ is merged

---
+++ ext/scriptmodules/libretrocores/lr-mame2016-lightgun.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+rp_module_id="lr-mame2016-lightgun"
+rp_module_desc="MAME emulator - MAME 0.174 port for libretro"
+rp_module_help="ROM Extensions: .zip\n\nCopy your MAME roms to either $romdir/mame-libretro or\n$romdir/arcade"
+rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/mame/master/COPYING"
+rp_module_repo="git https://github.com/StormedBubbles/mame2016-libretro.git master"
+rp_module_section="exp"
+rp_module_flags=""
+
+function __binary_url_lr-mame2016-lightgun() {
+    echo "https://github.com/obrie/retrokit/releases/download/latest/lr-mame2016-lightgun-rpi4-buster.tar.gz"
+}
+
+function install_bin_lr-mame2016-lightgun() {
+    downloadAndExtract "$(__binary_url_lr-mame2016-lightgun)" "$md_inst" --strip-components 1
+}
+
+function depends_lr-mame2016-lightgun() {
+    depends_lr-mame2016
+}
+
+function sources_lr-mame2016-lightgun() {
+    sources_lr-mame2016
+}
+
+function build_lr-mame2016-lightgun() {
+    build_lr-mame2016
+}
+
+function install_lr-mame2016-lightgun() {
+    install_lr-mame2016
+}
+
+function configure_lr-mame2016-lightgun() {
+    configure_lr-mame2016
+}
