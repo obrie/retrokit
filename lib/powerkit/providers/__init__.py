3c22abe7c2683a337215ad5598f812d23dbf3721
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-10-14 00:00:00 -0400

    powerkit: Add gpi2

--- lib/powerkit/providers/__init__.py
+++ lib/powerkit/providers/__init__.py
@@ -1,5 +1,6 @@
 from powerkit.providers.base import BaseProvider
 from powerkit.providers.argon1 import Argon1
 from powerkit.providers.gpi import GPi
+from powerkit.providers.gpi2 import GPi2
 from powerkit.providers.hotkey import Hotkey
 from powerkit.providers.nespi import Nespi
---
+++ lib/powerkit/providers/gpi2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from powerkit.providers import GPi
+
+class GPi2(GPi):
+    name = 'gpi2'
