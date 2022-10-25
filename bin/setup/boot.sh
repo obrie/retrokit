7edf34c99d3ce45d22342f4b3f2fbaf045e187b6
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-10-25 00:00:00 -0400

    boot: Fix device tree settings not being added in the correct sections

--- bin/setup/boot.sh
+++ bin/setup/boot.sh
@@ -12,12 +12,12 @@ configure() {
 }
 
 __configure_bios() {
-  # Note that we can't use crudini for dtoverlay additions because it doesn't
+  # Note that we can't use crudini for dtoverlay/dtparam additions because it doesn't
   # support repeating the same key multiple times in the same section
 
   local case=$(setting '.hardware.case.model')
 
-  # Build base config.txt without dtoverlay/dtparam settings
+  # Build base config.txt without device tree settings settings (dtoverlay / dtparam)
   ini_merge '{config_dir}/boot/config.txt' "$tmp_ephemeral_dir/boot-staging.txt" space_around_delimiters=false backup=false overwrite=true
   ini_merge "{config_dir}/boot/config/$case.txt" "$tmp_ephemeral_dir/boot-staging.txt" space_around_delimiters=false backup=false
   sed -i '/^dt\(overlay\|param\)=/d' "$tmp_ephemeral_dir/boot-staging.txt"
@@ -28,16 +28,37 @@ __configure_bios() {
   # Add repeating dtoverlay/dtparam configurations since crudini will just merge
   while read config_path; do
     while read section_name; do
-      local section_content
+      # Extract the specific source section that we're going to grep and where
+      # matches should get inserted into the target
+      local source_section_content
+      local target_section_start
       if [ "$section_name" == 'DEFAULT' ]; then
-        section_content=$(sed -n "1,/^\[/p" "$config_path")
+        source_section_content=$(sed -n "1,/^\[/p" "$config_path")
+        target_section_start=$(grep -En "^\[" /boot/config.txt | head -n 1 | cut -d':' -f1)
+        target_section_start=$((target_section_start-1))
       else
-        section_content=$(sed -n "/^\[$section_name\]/,/^\[/p" "$config_path")
+        source_section_content=$(sed -n "/^\[$section_name\]/,/^\[/p" "$config_path")
+        target_section_start=$(grep -En "^\[$section_name\]" /boot/config.txt | head -n 1 | cut -d':' -f1)
       fi
 
-      grep -E '^dt(overlay|param)=' "$config_path" | sudo tee -a /boot/config.txt
+      # Find device tree configurations in the section
+      local dt_content=$(echo "$source_section_content" | grep -E '^dt(overlay|param)=')
+      if [ -z "$dt_content" ]; then
+        continue
+      fi
+
+      # Write configurations to the section
+      echo "$dt_content" > "$tmp_ephemeral_dir/dtcontent.txt"
+      sudo sed -i "$target_section_start r $tmp_ephemeral_dir/dtcontent.txt" /boot/config.txt
     done < <(crudini --get "$config_path")
   done < <(cat <(each_path '{config_dir}/boot/config.txt') <(each_path "{config_dir}/boot/config/$case.txt"))
+
+  # Move [all] section to end of the file in order to comply with recommendations
+  # from Raspbian's documentation
+  local all_section_content=$(sed '1,/\[all\]/d;/\[/,$d' /boot/config.txt)
+  sudo sed -i '/^\[all\]/,/^\[/{//p;d;}' /boot/config.txt
+  sudo sed -i '/\[all\]/d' /boot/config.txt
+  echo -e "\n[all]\n$all_section_content" | sudo tee -a /boot/config.txt >/dev/null
 
   __configure_bios_ir
 }
@@ -51,6 +72,7 @@ __configure_bios_ir() {
     local ir_keymap_filename=$(basename "$ir_keymap_path")
     local rc_map_name=$(grep "$ir_keymap_filename" '/etc/rc_maps.cfg' | tr $'\t' ' ' | cut -d' ' -f 2)
 
+    # We are guaranteed that [all] is the last section
     echo "dtoverlay=gpio-ir,gpio_pin=$ir_gpio_pin,rc-map-name=$rc_map_name" | sudo tee -a /boot/config.txt
   fi
 }
