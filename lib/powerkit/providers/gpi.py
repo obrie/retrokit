4498865db1ca2c88a67bd8b4f9cdab6605c586c0
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-10-26 00:00:00 -0400

    powerkit: Fix gpi provider to not set a hold time

--- lib/powerkit/providers/gpi.py
+++ lib/powerkit/providers/gpi.py
@@ -13,10 +13,10 @@ class GPi(BaseProvider):
 
     # Starts listening for button presses
     def run(self):
-        self.power = gpiozero.LED(self.POWEREN_PIN)
+        power = gpiozero.LED(self.POWEREN_PIN)
 
         # Mark pins as being ON
-        self.power.on()
+        power.on()
 
-        power_button = gpiozero.Button(self.POWER_PIN, hold_time=float(self.config['shutdown']['hold_time']))
+        power_button = gpiozero.Button(self.POWER_PIN)
         power_button.when_pressed = self.shutdown
