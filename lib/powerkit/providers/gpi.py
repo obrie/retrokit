75d317f97289e5fa15d0398d65e7cc27bc4df6d3
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-10-27 00:00:00 -0400

    powerkit: Only disable pin closing after we've received the shutdown hook

--- lib/powerkit/providers/gpi.py
+++ lib/powerkit/providers/gpi.py
@@ -13,21 +13,24 @@ class GPi(BaseProvider):
 
     # Starts listening for button presses
     def run(self):
-        power = gpiozero.LED(self.POWEREN_PIN)
+        self.power = gpiozero.LED(self.POWEREN_PIN)
 
+        # Mark power as on (indicating we are doing a safe shutdown)
+        self.power.on()
+
+        self.power_button = gpiozero.Button(self.POWER_PIN)
+        self.power_button.when_pressed = self.shutdown
+
+    def shutdown(self):
         # Disable cleanup.  If we don't do this, then the power pin will be reset
         # to LOW, causing the system power to be cut before the system has safely
         # shut down.
         # 
         # See: https://github.com/gpiozero/gpiozero/issues/707
-        power.pin.close = self._close_pin
-        power.pin_factory.close = self._close_pin
+        self.power.pin.close = self._close_pin
+        self.power.pin_factory.close = self._close_pin
 
-        # Mark power as on (indicating we are doing a safe shutdown)
-        power.on()
-
-        power_button = gpiozero.Button(self.POWER_PIN)
-        power_button.when_pressed = self.shutdown
+        super().shutdown()
 
     def _close_pin(self, *args, **kwargs):
         pass
