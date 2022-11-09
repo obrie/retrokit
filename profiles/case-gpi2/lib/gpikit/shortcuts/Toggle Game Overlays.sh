d7398c464bea42363ddf370d96df298b41c97322
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-11-09 00:00:00 -0400

    case-gpi2: Refactor shortcut scripts

--- profiles/case-gpi2/lib/gpikit/shortcuts/Toggle Game Overlays.sh
+++ profiles/case-gpi2/lib/gpikit/shortcuts/Toggle Game Overlays.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 # Toggles whether RetroArch overlays are displayed
+toggle_overlays() {
+  retroarch_config_path=/opt/retropie/configs/all/retroarch.cfg
+  setting=input_overlay_enable
 
-retroarch_config_path=/opt/retropie/configs/all/retroarch.cfg
-setting=input_overlay_enable
+  if grep -q "^$setting *= *true" "$retroarch_config_path"; then
+    __swap_config true false
+    echo 'Overlays disabled'
+  else
+    __swap_config false true
+    echo 'Overlays enabled'
+  fi
+}
 
-swap_config() {
+__swap_config() {
   local from_value=$1
   local to_value=$2
   sed -i "s/^$setting *= *$from_value/$setting = $to_value/g" "$retroarch_config_path"
 }
 
-if grep -q "^$setting *= *true" "$retroarch_config_path"; then
-  swap_config true false
-  echo 'Overlays disabled'
-else
-  swap_config false true
-  echo 'Overlays enabled'
-fi
+toggle_overlays
--- profiles/case-gpi2/lib/gpikit/shortcuts/Toggle Stereo Output.sh
+++ profiles/case-gpi2/lib/gpikit/shortcuts/Toggle Stereo Output.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Toggles between mixing stereo for mono output and playing stereo output
-if [ -f /etc/asound.conf ]; then
-  rm /etc/asound.conf
-  echo 'Mono output enabled (mixed)'
-else
-  ln -fs /etc/asound-mono.conf /etc/asound.conf
-  echo 'Stereo output enabled'
-fi
+toggle_stereo_output() {
+  if [ -f /etc/asound.conf ]; then
+    rm /etc/asound.conf
+    echo 'Mono output enabled (mixed)'
+  else
+    ln -fs /etc/asound-mono.conf /etc/asound.conf
+    echo 'Stereo output enabled'
+  fi
+}
+
+toggle_stereo_output
