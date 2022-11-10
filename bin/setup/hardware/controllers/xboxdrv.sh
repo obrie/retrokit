143635eb5533499f8d27456037ad4deb505ec1b6
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-11-10 00:00:00 -0400

    xboxdrv: Simplify starting systemd services when a usb controller is plugged in

--- bin/setup/hardware/controllers/xboxdrv.sh
+++ bin/setup/hardware/controllers/xboxdrv.sh
@@ -15,20 +15,15 @@ build() {
 }
 
 __build_services() {
-  # Parent service
-  file_cp '{config_dir}/xboxdrv/xboxdrv.service' /etc/systemd/system/xboxdrv.service as_sudo=true backup=false envsubst=false
-
-  # Dependents
   while read service_path; do
     local filename=$(basename "$service_path")
     file_cp "$service_path" "/etc/systemd/system/xboxdrv-$filename" as_sudo=true backup=false envsubst=false
-  done < <(each_path '{config_dir}/xboxdrv' find '{}' -name '*.service' | grep -Ev 'xboxdrv.service')
+  done < <(each_path '{config_dir}/xboxdrv' find '{}' -name '*.service')
 }
 
 configure() {
   __configure_xboxdrv
   __configure_udev_rules
-  __configure_systemd_services
 }
 
 __configure_xboxdrv() {
@@ -47,31 +42,11 @@ __configure_udev_rules() {
   sudo udevadm control --reload-rules && sudo udevadm trigger
 }
 
-__configure_systemd_services() {
-  sudo systemctl enable xboxdrv
-
-  while read service_path; do
-    local filename=$(basename "$service_path")
-    sudo systemctl enable "xboxdrv-$filename"
-  done < <(each_path '{config_dir}/xboxdrv' find '{}' -name '*.service' | grep -Ev 'xboxdrv.service')
-
-  # Start the service
-  sudo systemctl restart xboxdrv
-}
-
 restore() {
   # Remove configurations
   sudo rm -rfv \
     /etc/xboxdrv \
     /etc/udev/rules.d/99-xboxdrv*
-
-  # Stop systemd services
-  sudo systemctl stop xboxdrv || true
-  sudo systemctl disable xboxdrv || true
-  while read service_path; do
-    local filename=$(basename "$service_path")
-    sudo systemctl disable "xboxdrv-$filename"
-  done < <(find /etc/systemd/system -maxdepth 1 -name 'xboxdrv-*.service' | grep -Ev 'xboxdrv.service')
 
   # Reload udev
   sudo udevadm control --reload-rules && sudo udevadm trigger
