2727b8a18c48c4cc67b6722d647f652897655fb4
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2022-12-10 00:00:00 -0400

    system-launchimages: Allow system-specific overrides of launch image urls

--- bin/setup/system-launchimages.sh
+++ bin/setup/system-launchimages.sh
@@ -7,8 +7,8 @@ setup_module_id='system-launchimages'
 setup_module_desc='System-specific launch images to display while emulators are loading'
 
 configure() {
-  local use_launch_image=$(system_setting '.themes.launch_image')
-  if [ "$use_launch_image" == 'false' ]; then
+  local launch_image_url=$(system_setting '.themes.launch_image')
+  if [ "$launch_image_url" == 'false' ]; then
     echo 'No launch images configured'
     restore
     return
@@ -16,17 +16,22 @@ configure() {
 
   # Identify the theme being used
   local launch_theme=$(setting '.themes.launch_theme')
-  local launch_images_base_url=$(setting ".themes.library[] | select(.name == \"$launch_theme\") | .launch_images_base_url")
 
-  # Get the name of the platform (not guaranteed to be the system's name)
-  local default_platform=$(xmlstarlet select -t -m "*/system[name='$system']" -v 'platform' -n /etc/emulationstation/es_systems.cfg)
-  local platform=$(ini_get '{config_dir}/retropie/platforms.cfg' '' "${system}_theme")
-  platform=${platform:-$default_platform}
-  platform=${platform//\"/}
+  if [ -z "$launch_image_url" ]; then
+    local launch_images_base_url=$(setting ".themes.library[] | select(.name == \"$launch_theme\") | .launch_images_base_url")
+
+    # Get the name of the platform (not guaranteed to be the system's name)
+    local default_platform=$(xmlstarlet select -t -m "*/system[name='$system']" -v 'platform' -n /etc/emulationstation/es_systems.cfg)
+    local platform=$(ini_get '{config_dir}/retropie/platforms.cfg' '' "${system}_theme")
+    platform=${platform:-$default_platform}
+    platform=${platform//\"/}
+
+    launch_image_url=$(render_template "$launch_images_base_url" platform="$platform")
+  fi
 
   # Download the image (identified by theme in case the theme changes)
   local download_path="$retropie_system_config_dir/launching-extended-$launch_theme.png"
-  download "$(render_template "$launch_images_base_url" platform="$platform")" "$download_path"
+  download "$launch_image_url" "$download_path"
 
   # Promote it to the primary launch screen for the system
   ln_if_different "$download_path" "$retropie_system_config_dir/launching-extended.png"
