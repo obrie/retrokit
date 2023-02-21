16fb2acc13ce833b989afb63b8029da8f7ce0b5c
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2023-02-21 00:00:00 -0400

    cheats: Switch download file to be written to the ephemeral directory

--- bin/setup/cheats.sh
+++ bin/setup/cheats.sh
@@ -6,14 +6,18 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 setup_module_id='cheats'
 setup_module_desc='Retroarch cheats for use from the frontend'
 
+cheats_dir="$retropie_configs_dir/all/retroarch/cheats"
+
 build() {
-  local cheats_zip="$tmp_dir/cheats.zip"
-  download 'http://buildbot.libretro.com/assets/frontend/cheats.zip' "$cheats_zip"
-  unzip -u "$cheats_zip" -d "$retropie_configs_dir/all/retroarch/cheats/"
+  if ! find "$cheats_dir" -type f -name '*.cht' | head -n1 | grep -q . || [ "$FORCE_UPDATE" == 'true' ]; then
+    local cheats_zip="$tmp_ephemeral_dir/cheats.zip"
+    download 'http://buildbot.libretro.com/assets/frontend/cheats.zip' "$cheats_zip"
+    unzip -u "$cheats_zip" -d "$cheats_dir/"
+  fi
 }
 
 remove() {
-  rm -rfv "$retropie_configs_dir/all/retroarch/cheats/"*
+  rm -rfv "$cheats_dir/"*
 }
 
 setup "${@}"
