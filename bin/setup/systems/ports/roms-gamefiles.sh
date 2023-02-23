1767ea42adbd54f7bbc54f1183a112365f3026ad
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2023-02-23 00:00:00 -0400

    systems/ports/rom-gamefiles: Only install game files for ports that are installed

--- bin/setup/systems/ports/roms-gamefiles.sh
+++ bin/setup/systems/ports/roms-gamefiles.sh
@@ -9,9 +9,17 @@ setup_module_id='system/ports/roms-gamefiles'
 setup_module_desc='Port file installation, using files from the system-roms-download setup script'
 
 configure() {
+  # Look up which ports we have installed
+  declare -A installed_ports
+  while read port_name; do
+    installed_ports[$port_name]=1
+  done < <(romkit_cache_list | jq -r '.name')
+
   # Link over any optional game files
   while IFS=$'\t' read -r port_name target_name source_file; do
-    file_ln "$source_file" "$roms_dir/ports/$port_name/$target_name"
+    if [ "${installed_ports[$port_name]}" ]; then
+      file_ln "$source_file" "$roms_dir/ports/$port_name/$target_name"
+    fi
   done < <(__list_gamefiles)
 }
 
