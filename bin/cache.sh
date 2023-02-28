eb832bfca668c4d335b25932200a9c3c2a6c4e37
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2023-02-28 00:00:00 -0400

    cache: Add initial scripts for building emulator binaries

--- bin/cache.sh
+++ bin/cache.sh
@@ -9,7 +9,7 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 usage() {
   echo "usage:"
-  echo " $0 delete"
+  echo " $0 delete|build_emulator_binaries|sync_emulator_binaries"
   exit 1
 }
 
@@ -46,6 +46,82 @@ delete() {
 
   # Remove cached data
   rm -rfv "$delete_dir"/*
+}
+
+default_binary_packages=(lr-mame0222 lr-mame0244 lr-mame2016-lightgun lr-swanstation lr-yabasanshiro actionmax)
+
+# Build binaries for emulators that are added by retrokit
+build_emulator_binaries() {
+  local package=$1
+  local dist=buster
+  local platform=rpi4
+  if [ $# -gt 1 ]; then local "${@:2}"; fi
+
+  if [[ "$(id -u)" -ne 0 ]]; then
+    # We must run as sudo since the build  process can take a long time
+    echo "Script must be run under sudo. Try 'sudo $0'"
+    exit 1
+  fi
+
+  local packages
+  if [ -z "$package" ] || [ "$package" == 'all' ]; then
+    packages=("${default_binary_packages[@]}")
+  else
+    packages=("$package")
+  fi
+
+  # Ensure tmp directories are set up
+  if [ ! -d "$retropie_setup_dir/tmp" ]; then
+    mkdir -p "$retropie_setup_dir/tmp/archives"
+  fi
+
+  # Create initial image
+  local chroot_retropie_setup_dir="$retropie_setup_dir/tmp/build/builder/buster/home/pi/RetroPie-Setup"
+  if [ ! -d "$chroot_retropie_setup_dir" ]; then
+    sudo __gpg_signing_key= __builder_dists=$dist __builder_platforms=$platform "$retropie_setup_dir/retropie_packages.sh" builder chroot_build module
+  fi
+
+  # Copy modules over to the mounted RetroPie-Setup
+  local chroot_scriptmodules_dir="$chroot_retropie_setup_dir/ext/retrokit/scriptmodules"
+  mkdir -p "$chroot_scriptmodules_dir"
+  # cp -Rv "$ext_dir/scriptmodules/"* "$chroot_scriptmodules_dir/"
+
+  # Build packages
+  sudo __gpg_signing_key= __builder_dists=$dist __builder_platforms=$platform "$retropie_setup_dir/retropie_packages.sh" builder chroot_build module "${packages[@]}"
+}
+
+# Upload emulator binaries to github
+sync_emulator_binaries() {
+  local package=$1
+  local dist=buster
+  local platform=rpi4
+  if [ $# -gt 1 ]; then local "${@:2}"; fi
+
+  local packages
+  if [ -z "$package" ] || [ "$package" == 'all' ]; then
+    packages=("${default_binary_packages[@]}")
+  else
+    packages=("$package")
+  fi
+
+  for package in "${packages[@]}"; do
+    # Stage the archive file
+    local archive_dir="$retropie_setup_dir/tmp/archives/$dist/$platform/kms"
+    local archive_file
+    if [[ "$package" == lr-* ]]; then
+      archive_file="$archive_dir/libretrocores/$package.tar.gz"
+    else
+      archive_file="$archive_dir/emulators/$package.tar.gz"
+    fi
+
+    # Upload to github
+    if [ -f "$archive_file" ]; then
+      echo "Uploading $package-$platform-$dist.tar.gz"
+      local upload_file="$tmp_ephemeral_dir/$package-$platform-$dist.tar.gz"
+      cp "$archive_file" "$upload_file"
+      gh release upload latest "$upload_file" --clobber --repo obrie/retrokit
+    fi
+  done
 }
 
 if [[ $# -lt 1 ]]; then
