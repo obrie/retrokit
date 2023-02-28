0ad33b8a0264465f3aa1350b5a084fc8f9a9940c
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2023-02-28 00:00:00 -0400

    lr-mame0244: Fix repo specifying the git sha incorrectly

--- ext/scriptmodules/libretrocores/lr-mame0244.sh
+++ ext/scriptmodules/libretrocores/lr-mame0244.sh
@@ -4,7 +4,7 @@ rp_module_id="lr-mame0244"
 rp_module_desc="MAME emulator - MAME 0.244 port for libretro"
 rp_module_help="ROM Extensions: .zip\n\nCopy your MAME roms to either $romdir/mame-libretro or\n$romdir/arcade"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/mame/master/COPYING"
-rp_module_repo="git https://github.com/libretro/mame.git ee3942fc824edaf67768c249f2dd57ec3a20f4b5"
+rp_module_repo="git https://github.com/libretro/mame.git master ee3942fc824edaf67768c249f2dd57ec3a20f4b5"
 rp_module_section="exp"
 rp_module_flags=""
 
