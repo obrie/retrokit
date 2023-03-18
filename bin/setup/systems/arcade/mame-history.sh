04ca3b2d993f86f17006adffbb4b172bf89df8a9
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2023-03-18 00:00:00 -0400

    systems/arcade/mame-history: Fix history file not being downloaded properly for lr-mame

--- bin/setup/systems/arcade/mame-history.sh
+++ bin/setup/systems/arcade/mame-history.sh
@@ -77,8 +77,8 @@ __build_mame() {
   if has_emulator 'lr-mame'; then
     if [ ! -f "$bios_dir/mame/history/history.xml" ] || [ "$FORCE_UPDATE" == 'true' ]; then
       local historydat_file=$(mktemp -p "$tmp_ephemeral_dir")
-      local latest_version=$(wget 'https://archive.org/download/mame-support/mame-support_files.xml' | grep -oE 'historyxml[0-9]+\.zip' | grep -oE '[0-9]+')
-      download 'https://archive.org/download/mame-support/Support/Support-Files/historyxml${latest_version}.zip' "$historydat_file"
+      local latest_version=$(download 'https://archive.org/download/mame-support/mame-support_files.xml' | grep -oE 'historyxml[0-9]+\.zip' | grep -oE '[0-9]+')
+      download "https://archive.org/download/mame-support/Support/Support-Files/historyxml${latest_version}.zip" "$historydat_file"
 
       mkdir -p "$bios_dir/mame/history"
       unzip -oj "$historydat_file" -d "$bios_dir/mame/history/"
