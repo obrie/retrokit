bb97c2e1bcf6ab7e6a1dcdaefc784c34d4082bf7
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2023-07-26 00:00:00 -0400

    network-dhcpd: Use proper background options and account for different OS versions with different dhcpcd locations

--- bin/setup/network-dhcpd.sh
+++ bin/setup/network-dhcpd.sh
@@ -9,11 +9,19 @@ setup_module_desc='Network DHCPD configuration'
 configure() {
   file_cp '{config_dir}/network/dhcpd/dhcpd.conf' '/etc/dhcpd.conf' as_sudo=true
   file_cp '{config_dir}/network/dhcpd/wait.conf' '/etc/systemd/system/dhcpcd.service.d/wait.conf' as_sudo=true
+
+  if [ -f /usr/lib/dhcpcd5/dhcpcd ] && [ ! -f /usr/sbin/dhcpcd ]; then
+    sudo ln -s /usr/lib/dhcpcd5/dhcpcd /usr/sbin/dhcpcd
+  fi
 }
 
 restore() {
   restore_file '/etc/dhcpd.conf' as_sudo=true delete_src=true
   restore_file '/etc/systemd/system/dhcpcd.service.d/wait.conf' as_sudo=true delete_src=true
+
+  if [ -f /usr/lib/dhcpcd5/dhcpcd ] && [ -L /usr/sbin/dhcpcd ]; then
+    sudo rm /usr/sbin/dhcpcd
+  fi
 }
 
 setup "${@}"
