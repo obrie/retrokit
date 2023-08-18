32964821feaaf85c59189bdb2f6d90d9062e013b
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2023-08-18 00:00:00 -0400

    retroarch: Fix shared files not being cleaned up on re-configuration

--- bin/setup/retroarch.sh
+++ bin/setup/retroarch.sh
@@ -20,6 +20,7 @@ reconfigure_packages() {
 }
 
 configure() {
+  __restore_shared_configs
   __restore_config
 
   __configure_global_overrides
@@ -33,20 +34,25 @@ __configure_global_overrides() {
 
 __configure_shared_overrides() {
   while read shared_config_name; do
-    ini_merge "{config_dir}/retroarch/$shared_config_name.cfg" "$retropie_configs_dir/all/$shared_config_name.cfg" backup=false comments='^#include '
+    ini_merge "{config_dir}/retroarch/$shared_config_name.cfg" "$retropie_configs_dir/all/$shared_config_name.cfg" backup=false comments='^#include ' overwrite=true
   done < <(each_path '{config_dir}/retroarch' find '{}'  -name 'retroarch-*.cfg' -not -name 'retroarch-core-options*.cfg' -exec basename {} .cfg \; | sort | uniq)
 }
 
 __configure_overlays() {
   # These are our own custom file, so no need to back up
-  ini_merge '{config_dir}/retroarch/overlay/base.cfg' "$retroarch_default_overlay_config_file" backup=false
+  ini_merge '{config_dir}/retroarch/overlay/base.cfg' "$retroarch_default_overlay_config_file" backup=false overwrite=true
   file_cp '{config_dir}/retroarch/overlay/base.png' "$retroarch_default_overlay_image_file" backup=false
 
-  ini_merge '{config_dir}/retroarch/overlay/base-lightgun.cfg' "$retroarch_default_overlay_lightgun_config_file" backup=false
+  ini_merge '{config_dir}/retroarch/overlay/base-lightgun.cfg' "$retroarch_default_overlay_lightgun_config_file" backup=false overwrite=true
   file_cp '{config_dir}/retroarch/overlay/base-lightgun.png' "$retroarch_default_overlay_lightgun_image_file" backup=false
 }
 
 restore() {
+  __restore_shared_configs
+  __restore_config delete_src=true
+}
+
+__restore_shared_configs() {
   rm -fv \
     "$retroarch_default_overlay_config_file" \
     "$retroarch_default_overlay_lightgun_config_file" \
@@ -54,8 +60,6 @@ restore() {
     "$retroarch_default_overlay_lightgun_image_file"
 
   find "$retropie_configs_dir/all" -mindepth 1 -maxdepth 1 -name 'retroarch-*.cfg' -not -name 'retroarch-core-options*.cfg' -exec rm -fv '{}' +
-
-  __restore_config delete_src=true
 }
 
 __restore_config() {
