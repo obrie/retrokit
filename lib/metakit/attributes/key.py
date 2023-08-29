7b34fb03f9990cac3fcd5030872beffedc046499
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2023-08-29 00:00:00 -0400

    metakit: Fix filenames being changed across all systems when a group name change is detected (instead of just the system whose game has changed)

--- lib/metakit/attributes/key.py
+++ lib/metakit/attributes/key.py
@@ -13,14 +13,14 @@ class KeyAttribute(BaseAttribute):
 
     ROOT_PATH = Path(os.environ['RETROKIT_HOME'])
     CONFIG_PATHS = [
-        'config/systems/*/autoport/{group}.cfg',
-        'config/systems/*/retroarch/{group}.*',
-        'config/systems/*/retroarch/*/{group}.*',
-        'config/systems/daphne/retroarch/commands/{group}.commands',
+        'config/systems/{system}/autoport/{group}.cfg',
+        'config/systems/{system}/retroarch/{group}.*',
+        'config/systems/{system}/retroarch/*/{group}.*',
+        'config/systems/{system}/retroarch/commands/{group}.commands',
     ]
 
     EXTERNAL_PATHS = [
-        '.emulationstation/downloaded_media/*/manuals/.*/{group} (*'
+        '.emulationstation/downloaded_media/{system}/manuals/.*/{group} (*'
     ]
 
     def get(self, key, entry):
@@ -35,7 +35,7 @@ class KeyAttribute(BaseAttribute):
     def migrate(self, from_group: str, to_group: str, *args) -> None:
         # Move all config files that are committed to source
         for glob_template in self.CONFIG_PATHS:
-            glob = glob_template.format(group=from_group)
+            glob = glob_template.format(system=self.romkit.system.name, group=from_group)
 
             for group_config_path in self.ROOT_PATH.glob(f'**/{glob}'):
                 new_path = Path(str(group_config_path).replace(from_group, to_group))
@@ -44,7 +44,7 @@ class KeyAttribute(BaseAttribute):
 
         # Print commands for files that are outside the source code
         for glob_template in self.EXTERNAL_PATHS:
-            glob = glob_template.format(group=from_group)
+            glob = glob_template.format(system=self.romkit.system.name, group=from_group)
 
             for group_file_path in Path.home().glob(glob):
                 new_path = Path(str(group_file_path).replace(from_group, to_group))
